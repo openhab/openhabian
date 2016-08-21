387e10abf3e385d53b3729e795074b2a5289b602
Thomas Dietrich <Thomas.Dietrich@tu-ilmenau.de>
2016-08-22 00:58:07 +0200

    Add raspi-config and etckeeper

--- openhabian-build.sh
+++ openhabian-build.sh
@@ -2,8 +2,8 @@
 
 # Make sure only root can run our script
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 1>&2
-   exit 1
+  echo "This script must be run as root" 1>&2
+  exit 1
 fi
 
 /bin/bash clean.sh
@@ -16,5 +16,5 @@ rm -rf raspbian-ua-netinst-*.xz &>/dev/null
 
 for file in raspbian-ua-netinst-*.*
 do
-   mv -v "$file" "${file//raspbian/openhabian}"
+  mv -v "$file" "${file//raspbian/openhabian}"
 done
