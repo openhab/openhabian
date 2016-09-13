d9a08f389e1df3fcbd43182e666e0b7e4f6faa84
Thomas Dietrich <Thomas.Dietrich@tu-ilmenau.de>
2016-09-13 23:26:34 +0200

    Increase menu height

--- openhabian-setup.sh
+++ openhabian-setup.sh
@@ -95,7 +95,7 @@ calc_wt_size() {
   # NOTE: it's tempting to redirect stderr to /dev/null, so supress error
   # output from tput. However in this case, tput detects neither stdout or
   # stderr is a tty and so only gives default 80, 24 values
-  WT_HEIGHT=17
+  WT_HEIGHT=22
   WT_WIDTH=$(tput cols)
 
   if [ -z "$WT_WIDTH" ] || [ "$WT_WIDTH" -lt 60 ]; then
