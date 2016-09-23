18cefb1003590326e6c4ffa76126a4e9cb377273
Thomas Dietrich <Thomas.Dietrich@tu-ilmenau.de>
2016-09-23 13:43:44 +0200

    Add openhab2 snapshot signing key
    https://github.com/openhab/openhab-distro/issues/270#issuecomment-249044303

--- openhabian-setup.sh
+++ openhabian-setup.sh
@@ -242,13 +242,14 @@ openhab2_addrepo() {
   rm /etc/apt/sources.list.d/openhab2.list
   echo "deb https://openhab.ci.cloudbees.com/job/openHAB-Distribution/ws/distributions/openhab-offline/target/apt-repo/ /" >> /etc/apt/sources.list.d/openhab2.list
   echo "deb https://openhab.ci.cloudbees.com/job/openHAB-Distribution/ws/distributions/openhab-online/target/apt-repo/ /" >> /etc/apt/sources.list.d/openhab2.list
+  cond_redirect wget -O - http://www.openhab.org/keys/public-key-snapshots.asc | apt-key add -
   cond_redirect apt update
   if [ $? -eq 0 ]; then echo "OK"; else echo "FAILED"; exit 1; fi
 }
 
 openhab2_install() {
   echo -n "[openHABian] Installing openhab2-offline (force ignore auth)... "
-  cond_redirect apt --yes --force-yes install openhab2-offline
+  cond_redirect apt --yes install openhab2-offline
   if [ $? -eq 0 ]; then echo "OK"; else echo "FAILED"; exit 1; fi
 }
 
