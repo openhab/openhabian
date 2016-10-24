b5c45bd30ae40e2ad64fd853cef5ec32ed68076a
jupe76 <peter.juenger@hotmail.com>
2016-10-24 11:06:51 +0200

    finished v 0.1

--- openhabian-setup.sh
+++ openhabian-setup.sh
@@ -393,28 +393,47 @@ copysysroot2usb() {
   NEWROOTDEV=/dev/sda
   NEWROOTPART=/dev/sda1
 
-  INFOTEXT="DANGEROUS OPERATION, USE WITH PRECAUTION!\n
-This will move your system root from your SDCARD to an USB-Device like a ssd or an USB-Stick.\n
+  infotext="DANGEROUS OPERATION, USE WITH PRECAUTION!\n
+This will move your system root from your SDCARD to an USB-Device like a ssd or an USB-Stick to reduce wear and failure or the SDCARD.\n
 1.) Make a backup of your SDCARD
 2.) Remove all USB massstorage devices from your PI
 3.) Insert the USB device to be used for the new system root. THIS DEVICE WILL BE FULLY DELETED
 \n
 Do you want to continue on your own risk?"
 
-  if ! (whiptail --title "Move system root to /dev/sda1" --yesno "$INFOTEXT" 18 78) then
+  if ! (whiptail --title "Move system root to "$NEWROOTPART --yesno "$infotext" 18 78) then
     echo "canceled, exit status was $?."
+   return
   fi
 
+  #check if system root is on partion 2 of the SDCARD
+  if ! grep -q "root=/dev/mmcblk0p2" /boot/cmdline.txt; then
+    infotext="It seems as if your systemroot is not on the SDCARD.
+       ***Aborting, process cant be started***"
+    whiptail --title "System root not on SDCARD?" --msgbox "$infotext" 8 78
+    return
+  fi
+
+  #check if USB power is already set to 1A, otherwise set it there
   if grep -q -F 'max_usb_current=1' /boot/config.txt; then
      echo "max_usb_current already set, ok"
   else
      echo 'max_usb_current=1' >> /boot/config.txt
+
+     echo
+     echo "********************************************************************************"
      echo "REBOOT, run openhabian-setup.sh again and recall menu item 'Move sysroot to USB'"
+     echo "********************************************************************************"
      whiptail --title "Reboot needed!" --msgbox "USB had to be set to high power (1A) first. Please REBOOT and RECALL this menu item" 15 78
      exit
   fi
 
-exit
+  #inform user to be patient ....
+  infotext="After confirming with OK, system root will be moved.
+Please be patient, this will taking 5 to 15 min depending mainly on the speed of your USB-Device.
+When the process is finished, you will be informed via msgbox"
+
+  whiptail --title "Moving system root ...." --msgbox "$infotext" 14 78
   
   echo "stopping openhab"
   systemctl stop openhab2
@@ -431,25 +450,39 @@ exit
 
   echo "creating filesys on " $NEWROOTPART
   #create new filesystem on partion 1
-  mkfs.ext4 -F -L oh_ssd $NEWROOTPART
+  mkfs.ext4 -F -L oh_usb $NEWROOTPART
 
   echo "mounting new root " $NEWROOTPART
   mount $NEWROOTPART /mnt 
 
-  echo "copying root sys, please be patient. Depending on the speed of your usb device, this can take 10 to 15 min"
-  tar cf - --one-file-system --exclude=/mnt/* --exclude=/proc/* --exclude=/lost+found/* --exclude=/sys/* --exclude=/media/* --exclude=/dev/* --exclude=/tmp/* --exclude=/boot/* --exclude=/run/* / | ( cd /mnt; sudo tar xfp -) 
+  echo
+  echo "**********************************************************************************************************"
+  echo "copying root sys, please be patient. Depending on the speed of your USB device, this can take 10 to 15 min"
+  echo "**********************************************************************************************************"
 
+  #tar cf - --one-file-system --exclude=/mnt/* --exclude=/proc/* --exclude=/lost+found/* --exclude=/sys/* --exclude=/media/* --exclude=/dev/* --exclude=/tmp/* --exclude=/boot/* --exclude=/run/* / | ( cd /mnt; sudo tar xfp -) 
+  rsync -aAXH --info=progress2 --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/boot/*"} / /mnt
+
+  echo
   echo "adjusting fstab on new root"
   #adjust system root in fstab
   sed -i "s#/dev/mmcblk0p2 /#"$NEWROOTPART" /#" /mnt/etc/fstab
 
   echo "adjusting system root in kernel bootline"
+  #make a copy of the original cmdline
+  cp /boot/cmdline.txt /boot/cmdline.txt.sdcard
   #adjust system root in kernel bootline
   sed -i "s#root=/dev/mmcblk0p2#root="$NEWROOTPART"#" /boot/cmdline.txt
 
   echo
-  echo "OK, you re all set, please reboot"
+  echo "*************************************************************"
+  echo "OK, moving system root finished youre all set, PLEASE REBOOT"
+  echo "*************************************************************"
 
+  infotext="OK, moving system root finished. PLEASE REBOOT"
+  whiptail --title "Moving system root finished ...." --msgbox "$infotext" 8 78
+
+  return
 }
 
 homegear_setup() {
