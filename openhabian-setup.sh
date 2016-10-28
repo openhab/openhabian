d5252100562b9c225aa44c557868bf584219c738
Thomas Dietrich <Thomas.Dietrich@tu-ilmenau.de>
2016-10-28 19:21:25 +0200

    Add user openhab to groups dialout and tty

--- openhabian-setup.sh
+++ openhabian-setup.sh
@@ -269,6 +269,8 @@ openhab2_install() {
   echo -n "[openHABian] Installing openhab2-offline... "
   cond_redirect apt -y install openhab2-offline
   if [ $? -eq 0 ]; then echo "OK"; else echo "FAILED"; exit 1; fi
+  cond_redirect adduser openhab dialout
+  cond_redirect adduser openhab tty
 }
 
 openhab2_service() {
@@ -434,7 +436,7 @@ Please be patient. This will take 5 to 15 minutes, depending mainly on the speed
 When the process is finished, you will be informed via message box..."
 
   whiptail --title "Moving system root ..." --msgbox "$infotext" 14 78
-  
+
   echo "stopping openHAB"
   systemctl stop openhab2
 
@@ -453,14 +455,14 @@ When the process is finished, you will be informed via message box..."
   mkfs.ext4 -F -L oh_usb $NEWROOTPART
 
   echo "mounting new root " $NEWROOTPART
-  mount $NEWROOTPART /mnt 
+  mount $NEWROOTPART /mnt
 
   echo
   echo "**********************************************************************************************************"
   echo "copying root sys, please be patient. Depending on the speed of your USB device, this can take 10 to 15 min"
   echo "**********************************************************************************************************"
 
-  #tar cf - --one-file-system --exclude=/mnt/* --exclude=/proc/* --exclude=/lost+found/* --exclude=/sys/* --exclude=/media/* --exclude=/dev/* --exclude=/tmp/* --exclude=/boot/* --exclude=/run/* / | ( cd /mnt; sudo tar xfp -) 
+  #tar cf - --one-file-system --exclude=/mnt/* --exclude=/proc/* --exclude=/lost+found/* --exclude=/sys/* --exclude=/media/* --exclude=/dev/* --exclude=/tmp/* --exclude=/boot/* --exclude=/run/* / | ( cd /mnt; sudo tar xfp -)
   rsync -aAXH --info=progress2 --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/boot/*"} / /mnt
 
   echo
@@ -479,13 +481,13 @@ When the process is finished, you will be informed via message box..."
   echo "OK, moving system root finished you're all set, PLEASE REBOOT"
   echo
   echo "In the unlikely case that the reboot does not suceed,"
-  echo "please put the SD card into another device and copy back" 
+  echo "please put the SD card into another device and copy back"
   echo "/boot/cmdline.txt.sdcard to /boot/cmdline.txt"
   echo "*************************************************************"
 
   infotext="OK, moving system root finished. PLEASE REBOOT\n
 In the unlikely case that the reboot does not suceed,
-please put the SD card into another device and copy back 
+please put the SD card into another device and copy back
 /boot/cmdline.txt.sdcard to /boot/cmdline.txt"
   whiptail --title "Moving system root finished ...." --msgbox "$infotext" 12 78
 }
