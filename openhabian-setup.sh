b61bf0dd05fa5c6c5a77caf3fd88ac8b1cb13b04
Ben Clark <b.clark-09@hotmail.com>
2016-11-01 23:28:11 +0000

    Improve nginx dialogs in whiptail, fix dig problem (#27)
    - User input is now done by whiptail.
    - A confirmation of settings chosen is presented before any changes are
    made.
    - dig retrieves the last IP in it's lookup. Which should solve problems
    for CNAME > IP routes

--- openhabian-setup.sh
+++ openhabian-setup.sh
@@ -734,114 +734,159 @@ influxdb_grafana_setup() {
 }
 
 nginx_setup() {
-function comment {
-  sed -i "$1"' s/^/#/' "$2"
-}
-function uncomment {
-  sed -i "$1"' s/^ *#//' "$2"
-}
+  introtext="This will enable you to connect to openHAB through a HTTP port and optionally secure it with a username/password and an SSL certificate."
+  failtext="Sadly there was a problem setting up the selected option. Please report this problem in the openHAB community forum or as a openHABian GitHub issue."
 
-echo "Installing NGINX and DNS Utilities..."
-apt -y -q install nginx dnsutils
+  if [ -n "$INTERACTIVE" ]; then
+    if ! (whiptail --title "Description, Continue?" --yes-button "Continue" --no-button "Back" --yesno "$introtext" 15 80) then return 1; fi
+  fi
 
-AUTH=false
-SECURE=false
-VALIDDOMAIN=false
-matched=false
+  function comment {
+    sed -i "$1"' s/^/#/' "$2"
+  }
+  function uncomment {
+    sed -i "$1"' s/^ *#//' "$2"
+  }
 
-echo -n "Obtaining Public IP Address... "
-wanip=$(dig +short myip.opendns.com @resolver1.opendns.com)
-echo "$wanip"
+  echo "Installing DNS Utilities..."
+  apt -y -q install dnsutils
 
-read -r -p "Would you like to secure your openHAB interface with username and password? [y/N]: " response
-if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
-  read -p "Enter a username to sign into openHAB: " username
-  while [ "$matched" = false ]; do
-    read -s -p "Enter a password for $username: " password
-    echo ""
-    read -s -p "Please confirm the password: " secondpassword
-    echo ""
-    if [ "$password" = "$secondpassword" ]; then
-      matched=true
-      echo "Password accepted!"
+  AUTH=false
+  SECURE=false
+  VALIDDOMAIN=false
+  matched=false
+  canceled=false
+  FAILED=false
+
+  if (whiptail --title "Authentication Setup" --yesno "Would you like to secure your openHAB interface with username and password?" 15 80) then
+    username=$(whiptail --title "Authentication Setup" --inputbox "Enter a username to sign into openHAB:" 15 80 openhab 3>&1 1>&2 2>&3)
+    if [ $? = 0 ]; then
+      while [ "$matched" = false ] && [ "$canceled" = false ]; do
+        password=$(whiptail --title "Authentication Setup" --passwordbox "Enter a password for $username:" 15 80 3>&1 1>&2 2>&3)
+        secondpassword=$(whiptail --title "Authentication Setup" --passwordbox "Please confirm the password:" 15 80 3>&1 1>&2 2>&3)
+        if [ "$password" = "$secondpassword" ] && [ ! -z "$password" ]; then
+          matched=true
+          AUTH=true
+        else
+          password=$(whiptail --title "Authentication Setup" --msgbox "Password mismatched or blank... Please try again!" 15 80 3>&1 1>&2 2>&3)
+        fi
+      done
     else
-      echo "Password mismatch, please try again!"
+      canceled=true
     fi
-  done
-  AUTH=true
-fi
+  fi
 
-read -r -p "Would you like to secure your openHAB interface with HTTPS? [y/N]: " response
-if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
-  SECURE=true
-  echo -n "If you have a registered domain enter it now, otherwise leave blank: "
-  read domain
+  if (whiptail --title "Secure Certificate Setup" --yesno "Would you like to secure your openHAB interface with HTTPS?" 15 80) then
+    SECURE=true
+  fi
+
+  echo -n "Obtaining Public IP Address... "
+  wanip=$(dig +short myip.opendns.com @resolver1.opendns.com |tail -1)
+  echo "Public IP discovered as: $wanip"
+
+  domain=$(whiptail --title "Domain Setup" --inputbox "If you have a registered domain enter it now, otherwise leave blank:" 15 80 3>&1 1>&2 2>&3)
+
   while [ "$VALIDDOMAIN" = false ] && [ ! -z "$domain" ]; do
-    echo -n "Testing domain... "
-    domainip=$(dig +short $domain)
-    echo "resolves to $domainip"
+    domainip=$(dig +short $domain |tail -1)
     if [ "$wanip" = "$domainip" ]; then
-      echo "Domain is valid! Continuing!"
       VALIDDOMAIN=true
     else
-      echo -n "Domain does not resolve to your public IP address. Please enter a valid domain, leave blank to not use a domain name: "
-      read domain
+      domain=$(whiptail --title "Domain Setup" --inputbox "Domain does not resolve to your public IP address. Please enter a valid domain, leave blank to not use a domain name:" 15 80 3>&1 1>&2 2>&3)
     fi
   done
-fi
-if [ "$VALIDDOMAIN" = false ]; then
-  domain=$wanip
-  echo "Server name set to IP address..."
-fi
 
-rm -rf /etc/nginx/sites-enabled/default
-cp $SCRIPTDIR/includes/nginx.conf /etc/nginx/sites-enabled/openhab
-
-sed -i "s/DOMAINNAME/${domain}/g" /etc/nginx/sites-enabled/openhab
-
-if [ "$AUTH" = true ]; then
-  echo "Installing password utilities..."
-  apt -y -q install apache2-utils
-  echo "Creating password file..."
-  htpasswd -b -c /etc/nginx/.htpasswd $username $password
-  uncomment 32,33 /etc/nginx/sites-enabled/openhab
-fi
-
-if [ "$SECURE" = true ]; then
-  if [ "$VALIDDOMAIN" = true ]; then
-    echo "deb http://ftp.debian.org/debian jessie-backports main" > /etc/apt/sources.list.d/backports.list
-    gpg --keyserver pgpkeys.mit.edu --recv-key 8B48AD6246925553
-    gpg -a --export 8B48AD6246925553 | apt-key add -
-    gpg --keyserver pgpkeys.mit.edu --recv-key 7638D0442B90D010
-    gpg -a --export 7638D0442B90D010 | apt-key add -
-    apt update
-    echo "Installing Certbot..."
-    apt -y -q --force-yes install certbot -t jessie-backports
-    mkdir -p /var/www/$domain
-    uncomment 37,39 /etc/nginx/sites-enabled/openhab
-    nginx -t && service nginx reload
-    echo "Creating Let's Encrypt certificate..."
-    certbot certonly --webroot -w /var/www/$domain -d $domain #This will cause a prompt
-    certpath="/etc/letsencrypt/live/$domain/fullchain.pem"
-    keypath="/etc/letsencrypt/live/$domain/privkey.pem"
-    echo "Installing cron job for certificate renewal..."
-    HOUR=$((RANDOM % 11))
-    MINUTE=$((RANDOM % 59))
-    echo "$MINUTE $HOUR-23/12 * * * root certbot renew --quiet" > /etc/cron.d/certrenew
-  else
-    mkdir -p /etc/ssl/certs
-    certpath="/etc/ssl/certs/openhab.crt"
-    keypath="/etc/ssl/certs/openhab.key"
-    openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout $keypath -out $certpath #This will cause a prompt
+  if [ "$VALIDDOMAIN" = false ]; then
+    domain=$wanip
   fi
-  uncomment 20,21 /etc/nginx/sites-enabled/openhab
-  sed -i "s|CERTPATH|${certpath}|g" /etc/nginx/sites-enabled/openhab
-  sed -i "s|KEYPATH|${keypath}|g" /etc/nginx/sites-enabled/openhab
-  uncomment 6,10 /etc/nginx/sites-enabled/openhab
-  uncomment 15,17 /etc/nginx/sites-enabled/openhab
-  comment 14 /etc/nginx/sites-enabled/openhab
-fi
-nginx -t && service nginx reload && echo "OK"
+
+  if [ "$AUTH" = true ]; then
+    authtext="Authentication Enabled
+    - Username: $username"
+  else
+    authtext="Authentication Disabled"
+  fi
+
+  if [ "$SECURE" = true ]; then
+    httpstext="Proxy will be secured by HTTPS"
+    protocol="HTTPS"
+  else
+    httpstext="Proxy will not be secured by HTTPS"
+    protocol="HTTP"
+  fi
+
+  confirmtext="The following settings have been chosen:
+
+- $authtext
+- $httpstext
+- Server IP or Domain: $domain
+
+If you proceed, you will be able to connect to openHAB on the default $protocol port. Do you wish to continue and setup an NGINX server?"
+
+  if (whiptail --title "Confirmation" --yesno "$confirmtext" 15 80) then
+    echo "Installing nginx..."
+    apt -y -q install nginx || FAILED=true
+
+    rm -rf /etc/nginx/sites-enabled/default
+    cp $SCRIPTDIR/includes/nginx.conf /etc/nginx/sites-enabled/openhab
+
+    sed -i "s/DOMAINNAME/${domain}/g" /etc/nginx/sites-enabled/openhab
+
+    if [ "$AUTH" = true ]; then
+      echo "Installing password utilities..."
+      apt -y -q install apache2-utils || FAILED=true
+      echo "Creating password file..."
+      htpasswd -b -c /etc/nginx/.htpasswd $username $password
+      uncomment 32,33 /etc/nginx/sites-enabled/openhab
+    fi
+
+    if [ "$SECURE" = true ]; then
+      if [ "$VALIDDOMAIN" = true ]; then
+        echo "deb http://ftp.debian.org/debian jessie-backports main" > /etc/apt/sources.list.d/backports.list
+        gpg --keyserver pgpkeys.mit.edu --recv-key 8B48AD6246925553
+        gpg -a --export 8B48AD6246925553 | apt-key add -
+        gpg --keyserver pgpkeys.mit.edu --recv-key 7638D0442B90D010
+        gpg -a --export 7638D0442B90D010 | apt-key add -
+        apt update
+        echo "Installing Certbot..."
+        apt -y -q --force-yes install certbot -t jessie-backports
+        mkdir -p /var/www/$domain
+        uncomment 37,39 /etc/nginx/sites-enabled/openhab
+        nginx -t && service nginx reload
+        echo "Creating Let's Encrypt Certificate..."
+        certbot certonly --webroot -w /var/www/$domain -d $domain || FAILED=true #This will cause a prompt
+        if [ "$FAILED" = false ]; then
+          certpath="/etc/letsencrypt/live/$domain/fullchain.pem"
+          keypath="/etc/letsencrypt/live/$domain/privkey.pem"
+          echo "Installing cron job for certificate renewal..."
+          HOUR=$((RANDOM % 11))
+          MINUTE=$((RANDOM % 59))
+          echo "$MINUTE $HOUR-23/12 * * * root certbot renew --quiet" > /etc/cron.d/certrenew
+        fi
+      else
+        mkdir -p /etc/ssl/certs
+        certpath="/etc/ssl/certs/openhab.crt"
+        keypath="/etc/ssl/certs/openhab.key"
+        password=$(whiptail --title "openSSL Key Generation" --msgbox "openSSL is about to ask for information in the command line, please fill out each line." 15 80 3>&1 1>&2 2>&3)
+        openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout $keypath -out $certpath || FAILED=true #This will cause a prompt
+      fi
+      if [ "$FAILED" = false ]; then
+        uncomment 20,21 /etc/nginx/sites-enabled/openhab
+        sed -i "s|CERTPATH|${certpath}|g" /etc/nginx/sites-enabled/openhab
+        sed -i "s|KEYPATH|${keypath}|g" /etc/nginx/sites-enabled/openhab
+        uncomment 6,10 /etc/nginx/sites-enabled/openhab
+        uncomment 15,17 /etc/nginx/sites-enabled/openhab
+        comment 14 /etc/nginx/sites-enabled/openhab
+      fi
+    fi
+    nginx -t && service nginx reload || FAILED=true
+    if [ "$FAILED" = true ]; then
+      whiptail --title "Operation Failed!" --msgbox "$failtext" 15 80
+    else
+      whiptail --title "Operation Successful!" --msgbox "Setup successful. Please try entering $protocol://$domain in a browser to test your settings." 15 80
+    fi
+  else
+    whiptail --title "Operation Canceled!" --msgbox "Setup was canceled, no changes were made." 15 80
+  fi
 }
 
 openhabian_update() {
