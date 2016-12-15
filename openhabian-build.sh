938ad8d8c3b2cb568d7341090f472bd22bd4de73
Thomas Dietrich <Thomas.Dietrich@tu-ilmenau.de>
2016-12-15 21:17:05 +0100

    Make build script unaccessible to unwanted execution
    Signed-off-by: Thomas Dietrich <Thomas.Dietrich@tu-ilmenau.de>

--- openhabian-build.sh
+++ openhabian-build.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+echo "This script will build the openHABian Raspberry Pi image file."
+echo "That's probably not what you wanted to do."
+echo ""
+
+# Remove the following if you know what you are doing
+echo "Exiting."; exit 1
+
 # Make sure only root can run our script
 if [[ $EUID -ne 0 ]]; then
   echo "This script must be run as root" 1>&2
