85c258623aed7c9b26e1042296154a46e7266542
Thomas Dietrich <Thomas.Dietrich@tu-ilmenau.de>
2017-02-13 20:32:53 +0100

    Check apt readiness on boot
    Signed-off-by: Thomas Dietrich <Thomas.Dietrich@tu-ilmenau.de>

--- build-pine64-image/first-boot.sh
+++ build-pine64-image/first-boot.sh
@@ -10,16 +10,24 @@ rm -f /opt/openHABian-install-failed
 touch /opt/openHABian-install-inprogress
 
 echo "$(timestamp) [openHABian] Ensuring network connectivity... "
-sleep 60
-if ! ping -c 1 8.8.8.8 > /dev/null; then
-  echo "$(timestamp) [openHABian] Network unreachable, can't continue. Please reboot and let me try again."
-  exit 0
-fi
+cnt=0
+until ping -c1 8.8.8.8 &>/dev/null; do
+  sleep 1
+  cnt=$((cnt + 1))
+  if [ $cnt -eq 100 ]; then
+    echo "$(timestamp) [openHABian] Network unreachable, can't continue. Please reboot and let me try again."
+    exit 0
+  fi
+done
+
+echo -n "$(timestamp) [openHABian] Waiting for dpkg/apt to get ready... "
+until apt update &>/dev/null; do sleep 1; done
+echo "OK"
 
 echo -n "$(timestamp) [openHABian] Updating repositories and upgrading installed packages... "
 apt update &>/dev/null
 apt --yes upgrade &>/dev/null
-if [ $? -eq 0 ]; then echo "OK"; else echo "FAILED"; fi # exit 1; fi
+if [ $? -eq 0 ]; then echo "OK"; else echo "FAILED"; exit 1; fi
 
 echo -n "$(timestamp) [openHABian] Installing git package... "
 /usr/bin/apt-get -y install git &>/dev/null
