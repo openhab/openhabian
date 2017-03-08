5d04b5bc9e87b85e4f0b46734c083a1a69394013
Thomas Dietrich <Thomas.Dietrich@tu-ilmenau.de>
2017-03-08 01:14:26 +0100

    Add Raspbian build, Vagrantfile, timezone detection
    Signed-off-by: Thomas Dietrich <Thomas.Dietrich@tu-ilmenau.de>

--- build-pine64.sh
+++ build-pine64.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
+set -e
 
 echo "[openHABian] This script will build the openHABian Pine64 image file."
 if [ ! "$1" == "go" ]; then
   echo "That's probably not what you wanted to do. Exiting."
   exit 0
 fi
-echo ""
 
 # Make sure only root can run our script
 if [[ $EUID -ne 0 ]]; then
@@ -17,29 +17,35 @@ fi
 cd $(dirname $0) || exit 1
 
 timestamp() { date +"%F_%T_%Z"; }
+echo_process() { echo -e "\e[1;94m$(timestamp) [openHABian] $*\e[0m"; }
 
 # Log everything to a file
 exec &> >(tee -a "openhabian-build-$(date +%Y-%m-%d_%H%M%S).log")
 
-# Prerequisites
-apt update && apt --yes install git curl bzip2 zip xz-utils xz-utils build-essential binutils kpartx dosfstools bsdtar qemu-user-static qemu-user
-
-echo "$(timestamp) [openHABian] Cloning \"longsleep/build-pine64-image\" project... "
+# Load config, create temporary build folder
+sourcefolder=build-rpi-raspbian
+source $sourcefolder/openhabian.raspbian.conf
 buildfolder=/tmp/build-pine64-image
+rm -rf $buildfolder
+
+# Prerequisites
+apt update && apt --yes install git wget curl bzip2 zip xz-utils xz-utils build-essential binutils kpartx dosfstools bsdtar qemu-user-static qemu-user
+
+echo_process "Cloning \"longsleep/build-pine64-image\" project... "
 git clone -b master https://github.com/longsleep/build-pine64-image.git $buildfolder
 
-echo "$(timestamp) [openHABian] Downloading aditional files needed by \"longsleep/build-pine64-image\" project... "
+echo_process "Downloading aditional files needed by \"longsleep/build-pine64-image\" project... "
 wget -nv -P $buildfolder/ https://www.stdin.xyz/downloads/people/longsleep/pine64-images/simpleimage-pine64-latest.img.xz
 wget -nv -P $buildfolder/ https://www.stdin.xyz/downloads/people/longsleep/pine64-images/linux/linux-pine64-latest.tar.xz
 
-echo "$(timestamp) [openHABian] Copying over 'rc.local' and 'first-boot.sh' for image integration... "
+echo_process "Copying over 'rc.local' and 'first-boot.sh' for image integration... "
 cp build-pine64-image/rc.local $buildfolder/simpleimage/openhabianpine64.rc.local
 cp build-pine64-image/first-boot.sh $buildfolder/simpleimage/openhabianpine64.first-boot.sh
-#cp build-pine64-image/openhabian.pine64.conf $buildfolder/simpleimage/openhabian.conf
+cp build-pine64-image/openhabian.pine64.conf $buildfolder/simpleimage/openhabian.conf
 
 source build-pine64-image/openhabian.pine64.conf
 
-echo "$(timestamp) [openHABian] Hacking \"build-pine64-image\" build and make script... "
+echo_process "Hacking \"build-pine64-image\" build and make script... "
 #sed -i "s/date +%Y%m%H/date +%Y%m%d%H/" $buildfolder/build-pine64-image.sh # Fix https://github.com/longsleep/build-pine64-image/pull/47 - Fixed!
 sed -i "s/date +%Y%m%d_%H%M%S_%Z/date +%Y%m%d%H/" $buildfolder/build-pine64-image.sh
 makescript=$buildfolder/simpleimage/make_rootfs.sh
@@ -47,22 +53,23 @@ makescript=$buildfolder/simpleimage/make_rootfs.sh
 sed -i "s/^pine64$/openHABianPine64/" $makescript
 sed -i "s/127.0.1.1 pine64/127.0.1.1 openHABianPine64/" $makescript
 sed -i "s/DEBUSER=ubuntu/DEBUSER=$username/" $makescript
+sed -i "s/DEBUSERPW=ubuntu/DEBUSERPW=$userpw/" $makescript
 echo -e "\n# Add openHABian modifications" >> $makescript
 echo "touch \$DEST/opt/openHABian-install-inprogress" >> $makescript
 echo "cp ./openhabianpine64.rc.local \$DEST/etc/rc.local" >> $makescript
 echo "cp ./openhabianpine64.first-boot.sh \$BOOT/first-boot.sh" >> $makescript
-#echo "cp ./openhabian.conf \$BOOT/openhabian.conf" >> $makescript
+echo "cp ./openhabian.conf \$BOOT/openhabian.conf" >> $makescript
 echo "echo \"openHABian preparations finished, /etc/rc.local in place\"" >> $makescript
 
-echo "$(timestamp) [openHABian] Executing \"build-pine64-image\" build script... "
+echo_process "Executing \"build-pine64-image\" build script... "
 (cd $buildfolder; /bin/bash build-pine64-image.sh simpleimage-pine64-latest.img.xz linux-pine64-latest.tar.xz xenial)
 if [ $? -eq 0 ]; then echo "OK"; else echo "FAILED"; fi
 
-echo -e "$(timestamp) [openHABian] Moving image and cleaning up... "
+echo_process "Moving image and cleaning up... "
 mv $buildfolder/xenial-pine64-*.img .
 rm -rf $buildfolder
 
-echo -e "$(timestamp) [openHABian] Renaming image... "
+echo_process "Renaming image... "
 for file in xenial-*.img; do
   mv -v "$file" "${file//pine64-bspkernel/openhabianpine64}"
 done
@@ -71,12 +78,12 @@ for file in xenial-*.img; do
   mv -v "$file" "${file//-1.img/-git$shorthash.img}"
 done
 
-echo -e "$(timestamp) [openHABian] Compressing image... "
+echo_process "Compressing image... "
 for file in xenial-*.img; do
   xz --verbose --compress --keep $file
 done
 
-echo -e "$(timestamp) [openHABian] Finished! The results:"
+echo_process "Finished! The results:"
 ls -alh xenial-*.*
 
 # vim: filetype=sh
---
+++ build-rpi-raspbian.sh
@@ -0,0 +1,87 @@
+#!/usr/bin/env bash
+set -e
+
+echo "[openHABian] This script will build the openHABian RPi (Raspbian Lite) image file."
+if [ ! "$1" == "go" ]; then
+  echo "That's probably not what you wanted to do. Exiting."
+  exit 0
+fi
+
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root" 1>&2
+  exit 1
+fi
+
+# Switch to the script folder
+cd $(dirname $0) || exit 1
+
+timestamp() { date +"%F_%T_%Z"; }
+echo_process() { echo -e "\e[1;94m$(timestamp) [openHABian] $*\e[0m"; }
+
+# Log everything to a file
+exec &> >(tee -a "openhabian-build-$(date +%Y-%m-%d_%H%M%S).log")
+
+# Load config, create temporary build folder, cleanup
+sourcefolder=build-rpi-raspbian
+source $sourcefolder/openhabian.raspbian.conf
+buildfolder=/tmp/build-raspbian-image
+umount $buildfolder/boot &>/dev/null || true
+umount $buildfolder/root &>/dev/null || true
+rm -rf $buildfolder
+
+# Prerequisites
+echo_process "Downloading prerequisites... "
+apt update
+apt --yes install git wget curl unzip kpartx
+
+echo_process "Downloading latest Raspbian Lite image... "
+mkdir $buildfolder
+if [ -f "raspbian.zip" ]; then
+  echo "(Using local copy...)"
+  cp raspbian.zip $buildfolder/raspbian.zip
+else
+  wget -nv -O $buildfolder/raspbian.zip "https://downloads.raspberrypi.org/raspbian_lite_latest"
+fi
+
+echo_process "Unpacking image... "
+unzip $buildfolder/raspbian.zip -d $buildfolder
+mv $buildfolder/*raspbian*.img $buildfolder/raspbian.img
+
+echo_process "Mounting the image for modifications... "
+mkdir -p $buildfolder/boot $buildfolder/root
+kpartx -asv $buildfolder/raspbian.img
+#dosfslabel /dev/mapper/loop0p1 "OPENHABIAN"
+mount -o rw -t vfat /dev/mapper/loop0p1 $buildfolder/boot
+mount -o rw -t ext4 /dev/mapper/loop0p2 $buildfolder/root
+
+echo_process "Setting hostname, reactivating SSH... "
+sed -i "s/127.0.1.1 .*/127.0.1.1 $hostname/" $buildfolder/root/etc/hosts
+echo "$hostname" > $buildfolder/root/etc/hostname
+touch $buildfolder/boot/ssh
+
+echo_process "Injecting 'rc.local', 'first-boot.sh' and 'openhabian.conf'... "
+cp $sourcefolder/rc.local $buildfolder/root/etc/rc.local
+cp $sourcefolder/first-boot.sh $buildfolder/boot/first-boot.sh
+cp $sourcefolder/openhabian.raspbian.conf $buildfolder/boot/openhabian.conf
+touch $buildfolder/root/opt/openHABian-install-inprogress
+
+echo_process "Closing up image file... "
+sync
+umount $buildfolder/boot
+umount $buildfolder/root
+kpartx -dv $buildfolder/raspbian.img
+
+echo_process "Moving image and cleaning up... "
+shorthash=$(git log --pretty=format:'%h' -n 1)
+destination="openhabianpi-raspbian-$(date +%Y%m%d%H%M)-git$shorthash.img"
+mv -v "$buildfolder/raspbian.img" $destination
+rm -rf $buildfolder
+
+echo_process "Compressing image... "
+xz --verbose --compress --keep $destination
+
+echo_process "Finished! The results:"
+ls -alh $destination*
+
+# vim: filetype=sh
