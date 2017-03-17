438b3b304a65d96719e46f720419f648be96a38e
Thomas Dietrich <Thomas.Dietrich@tu-ilmenau.de>
2017-03-17 16:44:43 +0100

    Update pine64 build with config and wifi
    Signed-off-by: Thomas Dietrich <Thomas.Dietrich@tu-ilmenau.de>

--- build-pine64-image/first-boot.sh
+++ build-pine64-image/first-boot.sh
@@ -9,6 +9,23 @@ echo "$(timestamp) [openHABian] Booting for the first time!"
 rm -f /opt/openHABian-install-failed
 touch /opt/openHABian-install-inprogress
 
+echo "$(timestamp) [openHABian] Storing configuration... "
+cp /boot/openhabian.conf /etc/openhabian.conf
+source /etc/openhabian.conf
+
+# While setup: show log to logged in user, will be overwritten by openhabian-setup.sh
+echo "watch -n 1 cat /boot/first-boot.log" > "/home/$username/.bash_profile"
+
+echo -n "$(timestamp) [openHABian] Setting up Wifi connection... "
+if [ -z ${wifi_ssid+x} ]; then
+  echo "SKIPPED"
+else
+  echo ""
+  echo -e "ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\nupdate_config=1" > /etc/wpa_supplicant/wpa_supplicant.conf
+  echo -e "network={\n\tssid=\"$wifi_ssid\"\n\tpsk=\"$wifi_psk\"\n}" >> /etc/wpa_supplicant/wpa_supplicant.conf
+  wpa_cli reconfigure &>/dev/null
+fi
+
 echo "$(timestamp) [openHABian] Ensuring network connectivity... "
 cnt=0
 until ping -c1 8.8.8.8 &>/dev/null; do
@@ -38,9 +55,9 @@ echo -n "$(timestamp) [openHABian] Cloning myself... "
 if [ $? -eq 0 ]; then echo "OK"; else echo "FAILED"; exit 1; fi
 ln -sfn /opt/openhabian/openhabian-setup.sh /usr/local/bin/openhabian-config
 
-echo -n "$(timestamp) [openHABian] Copying configuration and first boot script... "
-cp /opt/openhabian/build-pine64-image/openhabian.pine64.conf /etc/openhabian.conf
-if [ $? -eq 0 ]; then echo "OK"; else echo "FAILED"; exit 1; fi
+#echo -n "$(timestamp) [openHABian] Copying configuration and first boot script... "
+#cp /opt/openhabian/build-pine64-image/openhabian.pine64.conf /etc/openhabian.conf
+#if [ $? -eq 0 ]; then echo "OK"; else echo "FAILED"; exit 1; fi
 
 echo "$(timestamp) [openHABian] === Executing 'openhabian-setup.sh' ==="
 if (/bin/bash /opt/openhabian/openhabian-setup.sh unattended); then
