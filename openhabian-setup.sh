61d304238f057f1614b924fd9cc2b62f5d6f0668
Thomas Dietrich <Thomas.Dietrich@tu-ilmenau.de>
2017-06-06 21:49:15 +0200

    Add Pi0W as a device for wifi function
    Signed-off-by: Thomas Dietrich <Thomas.Dietrich@tu-ilmenau.de>

--- openhabian-setup.sh
+++ openhabian-setup.sh
@@ -681,9 +681,9 @@ Finally, all common serial ports can be made accessible to the openHAB java virt
 
 wifi_setup() {
   echo -n "$(timestamp) [openHABian] Setting up Wifi (PRi3 or Pine A64)... "
-  if ! is_pithree && ! is_pine64; then
+  if ! is_pithree && ! is_pizerow && ! is_pine64; then
     if [ -n "$INTERACTIVE" ]; then
-      whiptail --title "Incompatible Hardware Detected" --msgbox "Wifi setup: This option is for a Raspberry Pi 3 system only." 10 60
+      whiptail --title "Incompatible Hardware Detected" --msgbox "Wifi setup: This option is for the Pi3, Pi0W or the Pine A64 system only." 10 60
     fi
     echo "FAILED"; return 1
   fi
@@ -693,13 +693,14 @@ wifi_setup() {
     PASS=$(whiptail --title "Wifi Setup" --inputbox "What's the password for that Wifi?" 10 60 3>&1 1>&2 2>&3)
     if [ $? -ne 0 ]; then return 1; fi
   else
-    echo -n "setting default SSID and password in 'wpa_supplicant.conf' "
+    echo -n "Setting default SSID and password in 'wpa_supplicant.conf' "
     SSID="myWifiSSID"
     PASS="myWifiPassword"
   fi
   if is_pithree; then cond_redirect apt -y install firmware-brcm80211; fi
   cond_redirect apt -y install wpasupplicant wireless-tools
-  echo -e "ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\nupdate_config=1\nnetwork={\n  ssid=\"$SSID\"\n  psk=\"$PASS\"\n}" > /etc/wpa_supplicant/wpa_supplicant.conf
+  echo -e "ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\nupdate_config=1" > /etc/wpa_supplicant/wpa_supplicant.conf
+  echo -e "network={\n\tssid=\"$SSID\"\n\tpsk=\"$PASS\"\n}" >> /etc/wpa_supplicant/wpa_supplicant.conf
   if grep -q "wlan0" /etc/network/interfaces; then
     cond_echo ""
     cond_echo "Not writing to '/etc/network/interfaces', wlan0 entry already available. You might need to check, adopt or remove these lines."
@@ -707,13 +708,12 @@ wifi_setup() {
   else
     echo -e "\nallow-hotplug wlan0\niface wlan0 inet manual\nwpa-roam /etc/wpa_supplicant/wpa_supplicant.conf\niface default inet dhcp" >> /etc/network/interfaces
   fi
+  cond_redirect wpa_cli reconfigure
   cond_redirect ifdown wlan0
   cond_redirect ifup wlan0
-
   if [ -n "$INTERACTIVE" ]; then
     whiptail --title "Operation Successful!" --msgbox "Setup was successful. Your Wifi credentials were NOT tested. Please reboot now." 15 80
   fi
-
   echo "OK (Reboot needed)"
 }
 
