a21a57f01e691bd4bee3477108f13a28fbac4993
Leonid Kogan <leon@leonsio.com>
2018-01-12 14:30:04 +0100

    Checks angepasstz

--- build-yahm-lxc.sh
+++ build-yahm-lxc.sh
@@ -7,16 +7,32 @@ fail_inprogress() {
   exit 1
 }
 
+is_pizerow() {
+  grep -q "^Revision\s*:\s*[ 123][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]0[cC][0-9a-fA-F]$" /proc/cpuinfo
+  return $?
+} 
+
+is_pithree() {
+  grep -q "^Revision\s*:\s*[ 123][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]08[0-9a-fA-F]$" /proc/cpuinfo
+  return $?
+}
+
+
 # @todo need better checks: bridge installed? bridge name? this script works only with default values (yahmbr0)
-if [ -f /opt/YAHM/bin/yahm-ctl ]
+if [ ! -f /opt/YAHM/bin/yahm-ctl ]
 then
-    echo -n "$(timestamp) [openHABian] ERROR: Please install YAHM first: https://github.com/leonsio/YAHM"
+    echo "$(timestamp) [openHABian] ERROR: Please install YAHM first: https://github.com/leonsio/YAHM"
+    exit
+fi
+if [ -d /var/lib/lxc/openhab ]
+then
+    echo "$(timestamp) [openHABian] ERROR: Openhab LXC Instance found, please delete it first /var/lib/lxc/openhab"
     exit
 fi
 
-echo -n "$(timestamp) [GLOBAL] [openHABian] Starting the openHABian Host LXC installation."
+echo "\n$(timestamp) [GLOBAL] [openHABian] Starting the openHABian Host LXC installation.\n"
 
-echo -n "$(timestamp) [HOST] [openHABian] Updating repositories and upgrading installed packages... "
+echo -n "$(timestamp) [HOST] [openHABian] Updating repositories and upgrading installed packages."
 until apt update &>/dev/null; do sleep 1; done
 apt --yes upgrade &>/dev/null
 if [ $? -eq 0 ]; then echo "OK"; else echo "FAILED"; fail_inprogress; fi
@@ -32,16 +48,32 @@ if [ $? -eq 0 ]; then echo "OK"; else echo "FAILED"; fail_inprogress; fi
 echo -n "$(timestamp) [HOST] [openHABian] Creating LXC network configuration."
 yahm-network -n openhab -f attach_bridge &>/dev/null
 if [ $? -eq 0 ]; then echo "OK"; else echo "FAILED"; fail_inprogress; fi
-echo lxc.include=config.network >> /var/lib/lxc/openhab/config
+echo lxc.include=/var/lib/lxc/openhab/config.network >> /var/lib/lxc/openhab/config
 
 echo -n "$(timestamp) [HOST] [openHABian] Starting openhab LXC container."
 lxc-start -n openhab -d 
 if [ $? -eq 0 ]; then echo "OK"; else echo "FAILED"; fail_inprogress; fi
 
-echo -n "$(timestamp) [GLOBAL] [openHABian] Host Installation done, beginning with LXC preparation."
+echo "\n$(timestamp) [GLOBAL] [openHABian] Host Installation done, beginning with LXC preparation.\n"
 
 echo -n "$(timestamp) [LXC] [openHABian] Installing dependencies."
-lxc-attach -n openhab -- apt install -y git lsb-release &>/dev/null
+lxc-attach -n openhab -- apt install -y wget gnupg git lsb-release &>/dev/null
+if [ $? -eq 0 ]; then echo "OK"; else echo "FAILED"; fail_inprogress; fi
+
+if is_pithree || is_pizerow; then
+    echo "$(timestamp) [INFO] [openHABian] Raspberry Pi Hardware found, setup addition repositories."
+
+    echo -n "$(timestamp) [LXC] [openHABian] Installing repository key."
+    wget -qO - http://archive.raspberrypi.org/debian/raspberrypi.gpg.key  | lxc-attach -n openhab -- apt-key add - &>/dev/null
+    if [ $? -eq 0 ]; then echo "OK"; else echo "FAILED"; fail_inprogress; fi
+
+    echo -n "$(timestamp) [LXC] [openHABian] Installing repository files."
+    echo 'deb http://archive.raspberrypi.org/debian/ stretch main ui' | lxc-attach -n openhab  -- tee /etc/apt/sources.list.d/rpi.list &>/dev/null
+    if [ $? -eq 0 ]; then echo "OK"; else echo "FAILED"; fail_inprogress; fi
+fi
+
+echo -n "$(timestamp) [LXC] [openHABian] Creating gpio user."
+lxc-attach -n openhab -- useradd gpio &>/dev/null
 if [ $? -eq 0 ]; then echo "OK"; else echo "FAILED"; fail_inprogress; fi
 
 echo -n "$(timestamp) [LXC] [openHABian] Creating openhabian user."
@@ -64,12 +96,12 @@ echo -n "$(timestamp) [LXC] [openHABian] Creating openhabian default configurati
 echo -e "hostname=openHABian\nusername=openhabian\nuserpw=openhabian\ntimeserver=0.pool.ntp.org\nlocales='en_US.UTF-8 de_DE.UTF-8'\nsystem_default_locale=en_US.UTF-8\ntimezone=Europe/Berlin" > /var/lib/lxc/openhab/rootfs/etc/openhabian.conf &>/dev/null
 if [ $? -eq 0 ]; then echo "OK"; else echo "FAILED"; fail_inprogress; fi
 
-echo -n "$(timestamp) [LXC] [openHABian] Starting openhabian installation, this can take a lot of time....."
+echo  "$(timestamp) [LXC] [openHABian] Starting openhabian installation, this can take a lot of time....."
 lxc-attach -n openhab -- /opt/openhabian/openhabian-setup.sh unattended 
 
-echo -n "$(timestamp) [LXC] [openHABian] Starting openHAB2 Service."
+echo  "$(timestamp) [LXC] [openHABian] Starting openHAB2 Service."
 lxc-attach -n openhab -- systemctl start openhab2.service
 
-echo -n "$(timestamp) [GLOBAL] [openHABian] Done."
+echo  "\n$(timestamp) [GLOBAL] [openHABian] Done.\n"
 lxc-info -n openhab
 
