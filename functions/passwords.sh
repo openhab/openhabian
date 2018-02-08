00a2a987b5e8b6bf8dfdacd0319af2b74c084f05
Elias Gabrielsson <elias@gabson.se>
2018-02-08 21:25:33 +0100

    Nginx password change function and cleanup #326
    * Added support to change the password for nginx access user #326
    * Rewrite password menu to enable a dynamic view, eg. not all password are installed on all systems.
    * Updated documentaion to reflect reality concerning password handling.
    
    Signed-off-by: Elias Gabrielsson elias@benefactory.se (github: EliasGabrielsson)

--- functions/passwords.sh
+++ functions/passwords.sh
@@ -1,21 +1,40 @@
 #!/usr/bin/env bash
 
+## Function for changing various password related to openHAB or this system instance.
+## The function can be invoked either INTERACTIVE with userinterface UNATTENDED.
+##
+## When called UNATTENDED it will change all passwords. Notice that implies that the system is installed.
+##
+##    change_password(String password)
+##
+
 change_password() {
-  introtext="Choose which services to change the password for:"
+  introtext="Choose which services to change passwords for:"
   failtext="Something went wrong in the password change process. Please report this problem in the openHAB community forum or as a openHABian GitHub issue."
 
   matched=false
   canceled=false
+  allAccounts=("Linux system" "openHAB Console" "Samba" "Amanda backup")
   FAILED=0
+ 
+  # BUILD LIST WITH INSTALLED SERVICES
+  whipParams=( --title "Change password function" --ok-button "Execute" --cancel-button "Back" --checklist "$introtext" 20 90 10)
+  whipParams+=("Linux system" "Account; \"$username\" used for login to this computer" off )
+  whipParams+=("openHAB Console" "Remote console account; \"openhab\" for manage openHAB" off )
+  whipParams+=("Samba" "Fileshare account; \"$username\" for configuration files" off )
+  whipParams+=("Amanda backup" "User account; \"backup\" which could handle openHAB backups " off )
+
+  if [ -f /etc/nginx/.htpasswd ]; then
+    nginxuser=`cut -d: -f1 /etc/nginx/.htpasswd`
+    whipParams+=("Ngnix HTTP/HTTPS" "User; \"$nginxuser\" used for logon to openHAB web services " off )
+    allAccounts+="Ngnix HTTP/HTTPS"
+  fi 
 
   if [ -n "$INTERACTIVE" ]; then
-    accounts=$(whiptail --title "Choose accounts" --yes-button "Continue" --no-button "Back" --checklist "$introtext" 20 90 10 \
-          "Linux system" "Account; \"$username\" used to login to this computer" off \
-          "openHAB Console" "Remote console account; \"openhab\" for manage openHAB" off \
-          "Samba" "Fileshare account; \"$username\" for configuration files" off \
-          "Amanda backup" "User account; \"backup\" which could handle openhab backups " off \
-          3>&1 1>&2 2>&3)
+    accounts=$(whiptail "${whipParams[@]}" 3>&1 1>&2 2>&3)
     exitstatus=$?
+
+    # COLLECT NEW PASSWORD
     if [ $exitstatus = 0 ]; then
       while [ "$matched" = false ] && [ "$canceled" = false ]; do
         passwordChange=$(whiptail --title "Authentication Setup" --passwordbox "Enter a new password:" 15 80 3>&1 1>&2 2>&3)
@@ -32,34 +51,38 @@ change_password() {
       return 0
     fi
   else
+    # NON INTERACTIVE FUNCTION INVOKED
     passwordChange=$1
-    accounts=("Linux system" "openHAB Console" "Samba" "Amanda backup")
+    accounts=allAccounts
   fi
 
-  for i in "${accounts[@]}"
-  do
-    if [[ $i == *"Linux system"* ]]; then
-      echo -n "$(timestamp) [openHABian] Changing password for linux account \"$username\"... "
-      echo "$username:$passwordChange" | chpasswd
-      if [ $FAILED -eq 0 ]; then echo "OK"; else echo "FAILED"; fi
-    fi
-    if [[ $i == *"Samba"* ]]; then
-      echo -n "$(timestamp) [openHABian] Changing password for samba (fileshare) account \"$username\"... "
-      (echo "$passwordChange"; echo "$passwordChange") | /usr/bin/smbpasswd -s -a "$username"
-      if [ $FAILED -eq 0 ]; then echo "OK"; else echo "FAILED"; fi
-    fi
-    if [[ $i == *"openHAB Console"* ]]; then
-      echo -n "$(timestamp) [openHABian] Changing password for openHAB console account \"openhab\"... "
-      sed -i "s/openhab = .*,/openhab = $passwordChange,/g" /var/lib/openhab2/etc/users.properties
-      cond_redirect systemctl restart openhab2.service
-      if [ $FAILED -eq 0 ]; then echo "OK"; else echo "FAILED"; fi
-    fi
-	  if [[ $i == *"Amanda backup"* ]]; then
-      echo -n "$(timestamp) [openHABian] Changing password for linux account \"backup\"... "
-      echo "backup:$passwordChange" | chpasswd
-      if [ $FAILED -eq 0 ]; then echo "OK"; else echo "FAILED"; fi
-    fi
-  done
+  # CHANGE CHOOSEN PASSWORDS
+  if [[ $accounts == *"Linux system"* ]]; then
+    echo -n "$(timestamp) [openHABian] Changing password for linux account \"$username\"... "
+    echo "$username:$passwordChange" | chpasswd
+    if [ $FAILED -eq 0 ]; then echo "OK"; else echo "FAILED"; fi
+  fi
+  if [[ $accountsi == *"Samba"* ]]; then
+    echo -n "$(timestamp) [openHABian] Changing password for samba (fileshare) account \"$username\"... "
+    (echo "$passwordChange"; echo "$passwordChange") | /usr/bin/smbpasswd -s -a "$username"
+    if [ $FAILED -eq 0 ]; then echo "OK"; else echo "FAILED"; fi
+  fi
+  if [[ $accounts == *"openHAB Console"* ]]; then
+    echo -n "$(timestamp) [openHABian] Changing password for openHAB console account \"openhab\"... "
+    sed -i "s/openhab = .*,/openhab = $passwordChange,/g" /var/lib/openhab2/etc/users.properties
+    cond_redirect systemctl restart openhab2.service
+    if [ $FAILED -eq 0 ]; then echo "OK"; else echo "FAILED"; fi
+  fi
+  if [[ $accounts == *"Amanda backup"* ]]; then
+    echo -n "$(timestamp) [openHABian] Changing password for linux account \"backup\"... "
+    echo "backup:$passwordChange" | chpasswd
+    if [ $FAILED -eq 0 ]; then echo "OK"; else echo "FAILED"; fi
+  fi
+  if [[ $accounts == *"Ngnix HTTP/HTTPS"* ]]; then
+    echo -n "$(timestamp) [openHABian] Changing password for nginx web authentication account \"$nginxuser\"... "
+    echo "$passwordChange" | htpasswd -i /etc/nginx/.htpasswd $nginxuser
+    if [ $FAILED -eq 0 ]; then echo "OK"; else echo "FAILED"; fi
+  fi
 
   if [ -n "$INTERACTIVE" ]; then
     if [ $FAILED -eq 0 ]; then
