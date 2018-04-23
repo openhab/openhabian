57692ada4974042e9e46d63bea3fff5add19fe0e
mues-lee <37851178+mues-lee@users.noreply.github.com>
2018-04-23 19:06:19 +0200

    'function' renamed, code formatting improved
    + fixed a typo
    
    Signed-off-by: Marcus Krüger github@mues-lee.de (github: mues-lee)

--- build-rpi-raspbian/webif.sh
+++ build-rpi-raspbian/webif.sh
@@ -10,47 +10,47 @@ port=80 # Port the webserver is listing to
 source /etc/openhabian.conf # to get the hostname
 
 if [ $1 = "start" ]; then
-        mkdir /tmp/webif
-	ln -s /boot/first-boot.log /tmp/webif/first-boot.txt
-        echo "<html>
-		<head>
-		 <title>openHABian</title>
-		 <meta http-equiv="refresh" content="5" />
-		 <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
-		 <meta http-equiv="Pragma" content="no-cache" />
-		 <meta http-equiv="Expires" content="0" />
-		</head>" > /tmp/webif/index.html
-        echo "<body>
-		<h1>openHABian Installation Status</h1>
-		the log will be refreshed automatically every 10 seconds
-		<iframe src="http://$hostname:$port/first-boot.txt" scrolling="yes" width="100%" height="90%"></iframe>
-	       </body>
-	      </html>" >> /tmp/webif/index.html
-        python3 -m http.server $port > /dev/null 2>&1 &
+  mkdir /tmp/webif
+  ln -s /boot/first-boot.log /tmp/webif/first-boot.txt
+  echo "<html>
+        <head>
+        <title>openHABian</title>
+        <meta http-equiv="refresh" content="5" />
+        <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+        <meta http-equiv="Pragma" content="no-cache" />
+        <meta http-equiv="Expires" content="0" />
+        </head>" > /tmp/webif/index.html
+  echo "<body>
+        <h1>openHABian Installation Status</h1>
+        the log will be refreshed automatically every 10 seconds
+        <iframe src="http://$hostname:$port/first-boot.txt" scrolling="yes" width="100%" height="90%"></iframe>
+        </body>
+        </html>" >> /tmp/webif/index.html
+  python3 -m http.server $port > /dev/null 2>&1 &
 fi
 
-if [ $1 = "restart" ]; then
-	webifrunning=$(ps -ef | pgrep python3)
-	if [ -z $webifrunning ]; then
-		python3 -m http.server $port > /dev/null 2>&1 &
-	fi
+if [ $1 = "stillrunning" ]; then
+  webifrunning=$(ps -ef | pgrep python3)
+  if [ -z $webifrunning ]; then
+    python3 -m http.server $port > /dev/null 2>&1 &
+  fi
 fi
 
 if [ $1 = "inst_done" ]; then
- echo  "<html>
- 	<head>
-		<title>openHABian</title>
+  echo "<html>
+        <head>
+        <title>openHABian</title>
         </head>" > index.html
- echo "<body>
-	<h1>OpenHabian Installation Status</h1>
-	installation is done, you can now access the your openHAB Installation
-	using <a href="http://$hostname:8080">this link</a>
-	</body>
-	</html>" > /tmp/webif/index.html
+  echo "<body>
+        <h1>openHABian Installation Status</h1>
+        installation is done, you can now access the your openHAB Installation
+        using <a href="http://$hostname:8080">this link</a>
+        </body>
+        </html>" > /tmp/webif/index.html
 fi
 
 if [ $1 = "cleanup" ]; then
-	kill $(ps -ef | pgrep python3) > /dev/null && rm -R /tmp/webif > /dev/null
-	rm /boot/webif.sh > /dev/null
+  kill $(ps -ef | pgrep python3) > /dev/null && rm -R /tmp/webif > /dev/null
+  rm /boot/webif.sh > /dev/null
 fi
 
