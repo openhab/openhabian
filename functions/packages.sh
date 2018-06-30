2f6aa137a132d34540566cde89518181ce131619
Thomas Dietrich <Thomas.Dietrich@tu-ilmenau.de>
2018-06-30 14:58:22 +0200

    Update packages.sh
    Signed-off-by: Thomas Dietrich <thomas.dietrich@tu-ilmenau.de> (github: ThomDietrich)

--- functions/packages.sh
+++ functions/packages.sh
@@ -347,6 +347,7 @@ The article also contains instructions regarding openHAB integration.
     if ! (whiptail --title "Description, Continue?" --yes-button "Continue" --no-button "Back" --yesno "$introtext" 15 80) then echo "CANCELED"; return 0; fi
   fi
 
+  cond_redirect apt update
   cond_redirect apt -y install git python3 python3-pip bluetooth bluez
   if [ $FAILED -ne 0 ]; then echo "FAILED (prerequisites)"; exit 1; fi
   if [ ! -d "$DIRECTORY" ]; then
@@ -383,21 +384,26 @@ The article also contains instructions regarding openHAB integration.
 
 speedtest_cli_setup() {
   FAILED=0
-  introtext="This will install Speedtest CLI and Python. Soon this procedure will also set up the connection between them and with openHAB. For now, please follow the instructions found here:
-  \nhttps://community.openhab.org/t/7611/1"
+  introtext="This will install the Speedtest CLI tool. For integration with openHAB, please follow the instructions found here:
+  \nhttps://community.openhab.org/t/7611/1 \nSoon this procedure will set up the connection between it and openHAB automatically."
   failtext="Sadly there was a problem setting up the selected option. Please report this problem in the openHAB community forum or as a openHABian GitHub issue."
   successtext="Setup successful. Please continue with the instructions you can find here:\n\nhttps://community.openhab.org/t/7611/1"
 
-  echo "$(timestamp) [openHABian] Setting up Speedtest CLI... "
+  echo -n "$(timestamp) [openHABian] Setting up Speedtest CLI... "
 
-  cond_redirect apt -y install python-setuptools || FAILED=1
-  cond_echo ""
-  echo "Speedtest CLI... "
-  cond_redirect easy_install speedtest-cli || FAILED=1
-  cond_redirect apt update || FAILED=1
-
-  if [ $FAILED -eq 1 ]; then echo -n "FAILED "; else echo -n "OK "; fi
-  cond_echo ""
+  cond_redirect apt update
+  cond_redirect apt -y install python-setuptools
+  if [ $? -ne 0 ]; then echo "FAILED (prerequisites)"; exit 1; fi
+  cond_redirect easy_install speedtest-cli
+  if [ $? -ne 0 ]; then echo "FAILED"; else echo "OK"; exit 1; fi
+  
+  if [ -n "$INTERACTIVE" ]; then
+    if [ $FAILED -eq 0 ]; then
+      whiptail --title "Operation Successful!" --msgbox "$successtext" 15 80
+    else
+      whiptail --title "Operation Failed!" --msgbox "$failtext" 10 60
+    fi
+  fi
 }
 
 influxdb_grafana_setup() {
