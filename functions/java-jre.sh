2453dbfd2898229f2865aa953775f376c107f94e
Markus Storm <markus.storm@gmx.net>
2019-01-11 13:34:19 +0100

    corrected license filename

--- functions/java-jre.sh
+++ functions/java-jre.sh
@@ -31,7 +31,7 @@ java_zulu() {
     # Latest version check https://www.azul.com/downloads/zulu-linux
     local JAVA=zulu8.33.0.1-jdk8.0.192-linux_x64
   fi
-  whiptail --textbox $BASEDIR/includes/azul-zulu-license.md --scrolltext 27 116
+  whiptail --textbox $BASEDIR/includes/azul_zulu_license.md --scrolltext 27 116
   
   cond_redirect wget -nv -O $FILE http://cdn.azul.com/zulu-embedded/bin/${JAVA}.tar.gz
   cond_redirect tar -xpzf $FILE -C ${TEMPROOT}
