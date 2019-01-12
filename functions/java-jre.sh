8f928e914fcd50ee7655d2d18a6c05762ce491d0
Thomas Dietrich <Thomas.Dietrich@tu-ilmenau.de>
2019-01-12 09:59:40 +0100

    Temporarly revert #459 (#516)
    Signed-off-by: Thomas Dietrich <Thomas.Dietrich@tu-ilmenau.de>

--- functions/java-jre.sh
+++ functions/java-jre.sh
@@ -18,6 +18,27 @@ java_webupd8_archive() {
 }
 
 java_zulu() {
+  echo -n "$(timestamp) [openHABian] Installing Zulu Embedded OpenJDK... "
+  cond_redirect apt -y install dirmngr
+  if is_arm; then arch="[arch=armhf]"; fi
+  cond_redirect apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 219BD9C9
+  if [ $? -ne 0 ]; then echo "FAILED (keyserver)"; exit 1; fi
+  if is_ubuntu; then
+    echo "deb $arch http://repos.azulsystems.com/ubuntu stable main" > /etc/apt/sources.list.d/zulu-embedded.list
+  else  
+    echo "deb $arch http://repos.azulsystems.com/debian stable main" > /etc/apt/sources.list.d/zulu-embedded.list
+  fi
+  if is_pine64; then cond_redirect dpkg --add-architecture armhf; fi
+  cond_redirect apt update
+  if is_arm; then
+    cond_redirect apt -y install zulu-embedded-8
+  else
+    cond_redirect apt -y install zulu-8
+  fi
+  if [ $? -eq 0 ]; then echo "OK"; else echo "FAILED"; exit 1; fi
+}
+
+java_zulu_NEW() {
   local FILE
   local INSTALLROOT
   local TEMPROOT
