94a1c79bc44032288cc9235ec97f40f12f41f219
Markus Storm <markus.storm@gmx.net>
2019-03-09 10:42:25 +0100

    add ip capabilities to arping
    arping needs to be executed as either root or with these capabilities to have proper access to the network to work
    openHAB network binding to use it won't work without.
    
    Signed-off-by: Markus Storm markus.storm@gmx.net (github: mstormi)

--- functions/system.sh
+++ functions/system.sh
@@ -192,6 +192,7 @@ permissions_corrections() {
 misc_system_settings() {
   echo -n "$(timestamp) [openHABian] Applying miscellaneous system settings... "
   cond_redirect setcap 'cap_net_raw,cap_net_admin=+eip cap_net_bind_service=+ep' $(realpath /usr/bin/java)
+  cond_redirect /sbin/setcap cap_net_raw,cap_net_admin=eip  /usr/sbin/arping
   if is_pine64; then cond_redirect dpkg --add-architecture armhf; fi
   # user home note
   echo -e "This is your linux user's \"home\" folder.\nPlace personal files, programs or scripts here." > /home/$username/README.txt
