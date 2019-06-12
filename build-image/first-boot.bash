50ee298c6be6fcb09f4df993e43db27f2705b75b
Markus Storm <markus.storm@gmx.net>
2019-06-12 22:04:30 +0200

    run apt --fix-broken
    Signed-off-by: Markus Storm <markus.storm@gmx.net>

--- build-image/first-boot.bash
+++ build-image/first-boot.bash
@@ -132,6 +132,7 @@ echo "OK"
 
 echo -n "$(timestamp) [openHABian] Updating repositories and upgrading installed packages... "
 apt update &>/dev/null
+/usr/bin/apt --yes --fix-broken install &>/dev/null
 apt --yes upgrade &>/dev/null
 if [ $? -eq 0 ]; then echo "OK"; else echo "FAILED"; fail_inprogress; fi
 
