5d231b9b17023f07e7fb72b16ea9b9f9598b55b5
Markus Storm <markus.storm@gmx.net>
2020-04-29 23:10:08 +0200

    move apt-get update to background on openhabian-config start
    Signed-off-by: Markus Storm <markus.storm@gmx.net>

--- functions/java-jre.bash
+++ functions/java-jre.bash
@@ -53,13 +53,11 @@ java_zulu_8_tar(){
 
     if is_aarch64; then
       dpkg --add-architecture armhf
-      cond_redirect apt-get update
       cond_redirect apt-get --yes install libc6:armhf libncurses5:armhf libstdc++6:armhf
     fi
 
     if is_x86_64; then
       dpkg --add-architecture i386
-      cond_redirect apt-get update
       cond_redirect apt-get --yes install libc6:i386 libncurses5:i386 libstdc++6:i386
     fi
 
