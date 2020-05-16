4ddc64a8d989986af07698aa36e5a01c259984e3
Markus Storm <markus.storm@gmx.net>
2020-05-16 03:51:15 +0200

    fix double apt-update message
    Signed-off-by: Markus Storm <markus.storm@gmx.net>

--- functions/openhabian.bash
+++ functions/openhabian.bash
@@ -10,13 +10,12 @@ get_git_revision() {
 }
 
 apt_update() {
-  echo -n "$(timestamp) [openHABian] Updating Linux package information ... "
   apt-get -q update >/dev/null 2>&1 &
   PID_APT=$!
 }
 
 wait_for_apt_to_finish_update() {
-  echo -n "$(timestamp) [openHABian] Updating Linux package information ... "
+  echo "$(timestamp) [openHABian] Updating Linux package information ... "
   if [ ! -v PID_APT ]; then
     apt_update
   fi
