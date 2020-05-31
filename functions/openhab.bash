571d4756c12848d7a008aa07bc08738ae529ee20
Markus Storm <markus.storm@gmx.net>
2020-05-31 03:13:50 +0200

    don't exit installation/openhabian-config on nonfatal errors (#913)
    * replace exit by return on nonfatal errors so install/openhabian-config keeps going
    
    Signed-off-by: Markus Storm <markus.storm@gmx.net>

--- functions/openhab.bash
+++ functions/openhab.bash
@@ -74,10 +74,10 @@ Check the \"openHAB Release Notes\" and the official announcements to learn abou
     # - no support for other older distros
     APT_INST_OPTS="-y"
   fi
-  if ! cond_redirect apt-get ${APT_INST_OPTS} install "openhab2=${openhabVersion}"; then echo "FAILED (apt)"; exit 1; fi
+  if ! cond_redirect apt-get ${APT_INST_OPTS} install "openhab2=${openhabVersion}"; then echo "FAILED (apt)"; return 1; fi
   cond_redirect adduser openhab gpio
   cond_redirect systemctl daemon-reload
-  if cond_redirect systemctl enable --now openhab2; then echo "OK"; else echo "FAILED (usr)"; exit 1; fi
+  if cond_redirect systemctl enable --now openhab2; then echo "OK"; else echo "FAILED (service)"; return 1; fi
 
   if is_pi || is_pine64; then
     echo -n "$(timestamp) [openHABian] Optimizing Java to run on low memory single board computers... "
