9afd5971f689108ab32394f4f61a2e73e61f940d
Markus Storm <markus.storm@gmx.net>
2020-06-02 00:38:48 +0200

    cosmetics
    Signed-off-by: Markus Storm <markus.storm@gmx.net>

--- build-image/first-boot.bash
+++ build-image/first-boot.bash
@@ -178,8 +178,8 @@ echo "$(timestamp) [openHABian] Execution of 'openhabian-setup.sh unattended' co
 echo -n "$(timestamp) [openHABian] Waiting for openHAB to become ready on $HOSTNAME ..."
 
 # this took ~130 seconds on a RPi2
-if tryUntil "wget -S --spider -t 3 --waitretry=4 http://${HOSTNAME}:8080/start/index 2>&1 | grep -q 'HTTP/1.1 200 OK'" 20 10; then echo "failed."; exit 1; fi
-echo " OK"
+if tryUntil "wget -S --spider -t 3 --waitretry=4 http://${HOSTNAME}:8080/start/index 2>&1 | grep -q 'HTTP/1.1 200 OK'" 20 10; then echo "FAILED"; exit 1; fi
+echo "OK"
 
 echo "$(timestamp) [openHABian] Visit the openHAB dashboard now: http://${HOSTNAME:-openhab}:8080"
 echo "$(timestamp) [openHABian] To gain access to a console, simply reconnect."
--- functions/system.bash
+++ functions/system.bash
@@ -146,9 +146,6 @@ srv_bind_mounts() {
   systemctl is-active --quiet smbd >/dev/null 2>&1 && systemctl stop smbd
   systemctl is-active --quiet zram-config >/dev/null 2>&1 && systemctl stop zram-config
   cond_redirect umount -q /srv/openhab2-{sys,conf,userdata,logs,addons}
-#  sed -i "\\#[ \\t]/srv/openhab2-#d" /etc/fstab
-  # dummy " to fix vim coloring
-  cat /etc/fstab
   cond_redirect rm -f /etc/systemd/system/srv*.mount
   cond_redirect mkdir -p /srv/openhab2-{sys,conf,userdata,logs,addons}
   cond_redirect cp "$BASEDIR"/includes/srv_readme.txt /srv/README.txt
