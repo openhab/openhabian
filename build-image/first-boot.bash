050c0dfd3ada5e6286ceb3970ee51ae79fe21536
Markus Storm <markus.storm@gmx.net>
2020-07-03 14:46:44 +0200

    cosmetics
    Signed-off-by: Markus Storm <markus.storm@gmx.net>

--- build-image/first-boot.bash
+++ build-image/first-boot.bash
@@ -218,7 +218,7 @@ if [ -z "$SILENT" ]; then
   if [[ -d "$PID" ]]; then
     ps -auxq "$(cat "$PID")" | awk '/openhab/ {print "size/res="$5"/"$6" KB"}'
   else
-    echo -e "\\n${COL_RED}Karaf PID missing, openHAB process not (yet ?) running.\n"
+    echo -e "\\n${COL_RED}Karaf PID missing, openHAB process not (yet ?) running."
   fi
   echo -e "$COL_DEF"
 fi
