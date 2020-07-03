fe2ec4ed5e1b25fca98a807ededa7b54cf4e33b5
Markus Storm <markus.storm@gmx.net>
2020-07-03 11:59:13 +0200

    Update first-boot.bash
    Signed-off-by: Markus Storm markus.storm@gmx.net

--- build-image/first-boot.bash
+++ build-image/first-boot.bash
@@ -218,7 +218,7 @@ if [ -z "$SILENT" ]; then
   if [[ -d "$PID" ]]; then
     ps -auxq "$(cat "$PID")" | awk '/openhab/ {print "size/res="$5"/"$6" KB"}'
   else
-    printf "\n${COL_MAGENTA}Karaf PID missing, openHAB process not (yet ?) running.\n"
+    printf "\n%sKaraf PID missing, openHAB process not (yet ?) running.\n" "${COL_MAGENTA}"
   fi
   echo -e "$COL_DEF"
 fi
