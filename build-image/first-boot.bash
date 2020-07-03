a0e022d3aece5b8c465021b5013f41768a38b160
Markus Storm <markus.storm@gmx.net>
2020-07-03 11:52:02 +0200

    Update first-boot.bash
    SPE
    
    Signed-off-by: Markus Storm markus.storm@gmx.net

--- build-image/first-boot.bash
+++ build-image/first-boot.bash
@@ -216,9 +216,9 @@ if [ -z "$SILENT" ]; then
   PID="${OPENHAB_USERDATA}"/tmp/karaf.pid
   echo -e "\\n${COL_CYAN}Memory usage:" && free -m
   if [[ -d "$PID" ]]; then
-    ps -auxq "$(cat $PID)" | awk '/openhab/ {print "size/res="$5"/"$6" KB"}'
+    ps -auxq "$(cat "$PID")" | awk '/openhab/ {print "size/res="$5"/"$6" KB"}'
   else
-    echo "\\n${COL_MAGENTA}Karaf PID missing, openHAB process not (yet ?) running."
+    echo "\n${COL_MAGENTA}Karaf PID missing, openHAB process not (yet ?) running."
   fi
   echo -e "$COL_DEF"
 fi
