41c8037a1bca4123f426f05e76e6e4875117b038
Markus Storm <markus.storm@gmx.net>
2020-07-25 12:45:18 +0200

    force proper homegear service umask
    Signed-off-by: Markus Storm <markus.storm@gmx.net>

--- build.bash
+++ build.bash
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+###########################################################
+#### dummy: change this line to force another image build
+###########################################################
 
 usage() {
   echo -e "Usage: $(basename "$0") <platform> [dev-git|dev-url] <branch> <url>"
