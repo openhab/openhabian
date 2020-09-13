96011d624effbdce90c0193437b147c10e9bb936
Markus Storm <markus.storm@gmx.net>
2020-09-13 18:11:21 +0200

    query adminmail; fix input logic on exim install
    Signed-off-by: Markus Storm <markus.storm@gmx.net>

--- functions/packages.bash
+++ functions/packages.bash
@@ -104,21 +104,16 @@ exim_setup() {
   temp="$(mktemp "${TMPDIR:-/tmp}"/openhabian.XXXXX)"
 
   echo -n "$(timestamp) [openHABian] Beginning Mail Transfer Agent install and setup... "
-  if [[ -z $INTERACTIVE ]]; then
+  if [[ -n $INTERACTIVE ]]; then
     if (whiptail --title "Mail Transfer Agent installation" --yes-button "Begin" --no-button "Cancel" --yesno "$introtext" 17 80); then echo "OK"; else echo "CANCELED"; return 0; fi
-  fi
-
-  echo "$(timestamp) [openHABian] Configuring exim4-config... "
-  if [[ -z $INTERACTIVE ]]; then
     if dpkg-reconfigure exim4-config; then echo "OK"; else echo "CANCELED"; return 0; fi
 
     if ! smarthost=$(whiptail --title "Enter public mail service smarthost to relay your mails to" --inputbox "\\nEnter the list of smarthost(s) to use your account for" 9 80 "*.google.com" 3>&1 1>&2 2>&3); then echo "CANCELED"; return 0; fi
-    if ! smartport=$(whiptail --title "port number of the smarthost to relay your mails to" --inputbox "\\nEnter the port number of the smarthost to use" 9 80 "*.google.com" 3>&1 1>&2 2>&3); then echo "CANCELED"; return 0; fi
-    if ! relayuser=$(whiptail --title "Enter your public service mail user" --inputbox "\\nEnter the mail username of the public service to relay all outgoing mail to $smarthost" 9 80 "yourname@gmail.com" 3>&1 1>&2 2>&3); then echo "CANCELED"; return 0; fi
-    if ! relaypass=$(whiptail --title "Enter your public service mail password" --passwordbox "\\nEnter the password used to relay mail as ${relayuser}@${smarthost}" 9 80 3>&1 1>&2 2>&3); then echo "CANCELED"; return 0; fi
+    if ! smartport=$(whiptail --title "port number of the smarthost to relay your mails to" --inputbox "\\nEnter the port number of the smarthost to use" 9 80 "587" 3>&1 1>&2 2>&3); then echo "CANCELED"; return 0; fi
+    if ! relayuser=$(whiptail --title "Enter your public service mail user" --inputbox "\\nEnter the mail username of the public service to relay all outgoing mail to $smarthost" 9 80 "$relayuser" 3>&1 1>&2 2>&3); then echo "CANCELED"; return 0; fi
+    if ! relaypass=$(whiptail --title "Enter your public service mail password" --passwordbox "\\nEnter the password used to relay mail as ${relayuser}@${smarthost}" 9 80 i"$relaypass" 3>&1 1>&2 2>&3); then echo "CANCELED"; return 0; fi
+    if ! adminmail=$(whiptail --title "Enter your administration user's mail address" --passwordbox "\\nEnter the address to send system reports to" 9 80 3 "$adminmail" >&1 1>&2 2>&3); then echo "CANCELED"; return 0; fi
   else
-    # edit file /etc/exim4/update-exim4.conf.conf and run command update-exim4.conf
-    # OR
     sed -e "s|%INTERFACES|${interfaces}|g" -e "s|%SMARTHOST|${smarthost}|g" -e "s|%SMARTPORT|${smartport}|g" -e "s|%RELAYNETS|${relaynets}|g" "$updateEximTemplate" > "$eximConfig"
     update-exim4.conf
   fi
