b29e0d6c5aa3e98542ee760edc70e92463ec3fde
Markus Storm <markus.storm@gmx.net>
2020-09-17 15:47:51 +0200

    shellcheck backup change
    Signed-off-by: Markus Storm <markus.storm@gmx.net>

--- functions/backup.bash
+++ functions/backup.bash
@@ -472,7 +472,8 @@ setup_mirror_SD() {
   partprobe
   cond_redirect mke2fs -F -t ext4 "${dest}3"
   mkdir -p "${storageDir}"
-  if ! sed -e "s|%DEVICE|${dest}3|g" -e "s|%BKPDIR|${storageDir}|g" "${BASEDIR:-/opt/openhabian}"/includes/storage.mount >${targetDir}/storage.mount; then echo "FAILED (create storage mount)"; fi
+  # shellcheck disable=SC2154
+  if ! sed -e "s|%DEVICE|${dest}3|g" -e "s|%BKPDIR|${storageDir}|g" "${BASEDIR:-/opt/openhabian}"/includes/storage.mount > "${targetDir}"/storage.mount; then echo "FAILED (create storage mount)"; fi
   if ! cond_redirect systemctl enable --now storage.mount; then echo "FAILED (enable storage mount)"; return 1; fi
 
   if [[ -n $INTERACTIVE ]]; then
