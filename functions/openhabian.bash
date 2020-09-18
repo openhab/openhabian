f2b6dd9519d1a7b0a51e30078e489fc3fc8b61e5
Markus Storm <markus.storm@gmx.net>
2020-09-18 11:30:57 +0200

    clarify default password warning
    Signed-off-by: Markus Storm <markus.storm@gmx.net>

--- functions/openhabian.bash
+++ functions/openhabian.bash
@@ -201,7 +201,7 @@ system_check_default_password() {
   fi
   originalPassword="$(grep -w "$defaultUser" /etc/shadow | cut -d: -f2)"
   algo="$(echo "$originalPassword" | cut -d'$' -f2)"
-  introText="The default password was detected on your system! That is a serious security concern. Others or malicious programs in your subnet are able to gain root access!\\n\\nPlease set a strong password by typing the command 'passwd'!"
+  introText="The default password was detected on your system! That is a serious security concern. Bad guys or malicious programs in your subnet are able to gain root access!\\n\\nPlease set a strong password by typing the command 'passwd ${defaultUser}'!"
   salt="$(echo "$originalPassword" | cut -d'$' -f3)"
   export algo defaultPassword salt
   generatedPassword="$(perl -le 'print crypt("$ENV{defaultPassword}","\$$ENV{algo}\$$ENV{salt}\$")')"
