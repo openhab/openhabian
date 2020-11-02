893ac6bd64a24e760749a87de5e936d815c37532
Markus Storm <markus.storm@gmx.net>
2020-11-02 19:32:33 +0100

    mount /srv/* with rw forced
    Signed-off-by: Markus Storm <markus.storm@gmx.net>

--- functions/system.bash
+++ functions/system.bash
@@ -235,7 +235,7 @@ create_mount() {
   source="$1"
 
   echo -n "$(timestamp) [openHABian] Creating mount $source in '/srv/openhab2-${destination}'... "
-  if ! sed -e 's|%SRC|'"${source}"'|g; s|%DEST|'"${destination}"'|g' "${BASEDIR:-/opt/openhabian}"/includes/mount_template > /etc/systemd/system/"$mountPoint"; then echo "FAILED (sed)"; return 1; fi
+  if ! sed -e 's|%SRC|'"${source}"'|g; s|%DEST|'"${destination}"'|g' "${BASEDIR:-/opt/openhabian}"/includes/srv_mount_template > /etc/systemd/system/"$mountPoint"; then echo "FAILED (sed)"; return 1; fi
   if ! cond_redirect systemctl -q daemon-reload &> /dev/null; then echo "FAILED (daemon-reload)"; return 1; fi
   if cond_redirect systemctl enable --now "$mountPoint"; then echo "OK"; else echo "FAILED (enable service)"; return 1; fi
 }
