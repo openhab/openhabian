a82748f428807e63e752842b8a2a205d452dc916
Markus Storm <markus.storm@gmx.net>
2020-11-14 17:56:17 +0100

    [skip ci] force build
    Signed-off-by: Markus Storm <markus.storm@gmx.net>

--- build.bash
+++ build.bash
@@ -2,7 +2,7 @@
 set -e
 
 ####################################################################
-#### dummy: changed this line 10 times to force another image build
+#### dummy: changed this line 11 times to force another image build
 ####################################################################
 
 usage() {
