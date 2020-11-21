ef07cb14f7f47cfbaf90103c0177d1f00fd89396
Markus Storm <markus.storm@gmx.net>
2020-11-21 23:53:25 +0100

    prefetch comitup dependencies
    Signed-off-by: Markus Storm <markus.storm@gmx.net>

--- includes/offline-install-modifications.bash
+++ includes/offline-install-modifications.bash
@@ -18,9 +18,11 @@ apt-get --quiet install --download-only --yes libattr1-dev libc6 libstdc++6 \
   xz-utils software-properties-common man-db whiptail acl usbutils dirmngr \
   arping apt-transport-https bc sysstat jq moreutils avahi-daemon python3 \
   python3-pip python3-wheel python3-setuptools avahi-autoipd fontconfig \
-  comitup dnsmasq-base libgudev-1.0-0 libmbim-glib4 libmbim-proxy libmm-glib0 \
-  libndp0 libnm0 libqmi-glib5 libqmi-proxy libteamdctl0 modemmanager \
-  network-manager python3-blinker python3-click python3-colorama \
+  comitup dns-root-data dnsmasq-base javascript-common libcairo2 \
+  libgudev-1.0-0 libjs-jquery libmbim-glib4 libmbim-proxy libmm-glib0 \
+  libndp0 libnm0 libpixman-1-0 libqmi-glib5 libqmi-proxy libteamdctl0 \
+  libxcb-render0 libxcb-shm0 libxrender1 modemmanager  network-manager \
+  python3-blinker python3-cairo python3-click python3-colorama \
   python3-flask python3-itsdangerous python3-jinja2 python3-markupsafe \
   python3-networkmanager python3-pyinotify python3-simplejson python3-werkzeug
 source /opt/openhabian/functions/nodejs-apps.bash
