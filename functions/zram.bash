c4af6a5de71610e38fc4cdd8b7238bd431996ae6
Ethan Dye <mrtops03@gmail.com>
2020-12-09 19:24:52 -0700

    Remove zramsync from zram code (#1293)
    Fixes: #1287
    
    Signed-off-by: Ethan Dye <mrtops03@gmail.com>

--- functions/zram.bash
+++ functions/zram.bash
@@ -8,7 +8,7 @@
 ##
 install_zram_code() {
   local overlayfsGit="https://github.com/kmxz/overlayfs-tools"
-  local zramGit="https://github.com/mstormi/openhabian-zram"
+  local zramGit="https://github.com/ecdye/zram-config"
 
   echo -n "$(timestamp) [openHABian] Installing ZRAM code... "
   if ! cond_redirect mkdir -p "$1"; then echo "FAILED (create directory)"; return 1; fi
@@ -19,10 +19,10 @@ install_zram_code() {
     if ! cond_redirect git clone "$overlayfsGit" "$1"/overlayfs-tools; then echo "FAILED (clone overlayfs)"; return 1; fi
   fi
 
-  if [[ -d "${1}/openhabian-zram" ]]; then
-    if cond_redirect update_git_repo "${1}/openhabian-zram" "master"; then echo "OK"; else echo "FAILED (update zram)"; return 1; fi
+  if [[ -d "${1}/zram-config" ]]; then
+    if cond_redirect update_git_repo "${1}/zram-config" "openHAB2"; then echo "OK"; else echo "FAILED (update zram)"; return 1; fi
   else
-    if cond_redirect git clone "$zramGit" "$1"/openhabian-zram; then echo "OK"; else echo "FAILED (clone zram)"; return 1; fi
+    if cond_redirect git clone --branch "openHAB2" "$zramGit" "$1"/zram-config; then echo "OK"; else echo "FAILED (clone zram)"; return 1; fi
   fi
 }
 
@@ -64,11 +64,10 @@ init_zram_mounts() {
     if cond_redirect install -m 755 "$zramInstallLocation"/overlayfs-tools/overlay /usr/local/lib/zram-config/overlay; then echo "OK"; else echo "FAILED (install overlayfs)"; return 1; fi
 
     echo -n "$(timestamp) [openHABian] Setting up ZRAM... "
-    if ! cond_redirect install -m 755 "$zramInstallLocation"/openhabian-zram/zram-config /usr/local/sbin; then echo "FAILED (zram-config)"; return 1; fi
-    if ! cond_redirect install -m 755 "$zramInstallLocation"/openhabian-zram/zramsync /usr/local/sbin; then echo "FAILED (zramsync)"; return 1; fi
+    if ! cond_redirect install -m 755 "$zramInstallLocation"/zram-config/zram-config /usr/local/sbin; then echo "FAILED (zram-config)"; return 1; fi
     if ! cond_redirect install -m 644 "${BASEDIR:-/opt/openhabian}"/includes/ztab /etc/ztab; then echo "FAILED (ztab)"; return 1; fi
     if ! cond_redirect mkdir -p /usr/local/share/zram-config/log; then echo "FAILED (create directory)"; return 1; fi
-    if cond_redirect install -m 644 "$zramInstallLocation"/openhabian-zram/zram-config.logrotate /etc/logrotate.d/zram-config; then echo "OK"; else echo "FAILED (logrotate)"; return 1; fi
+    if cond_redirect install -m 644 "$zramInstallLocation"/zram-config/zram-config.logrotate /etc/logrotate.d/zram-config; then echo "OK"; else echo "FAILED (logrotate)"; return 1; fi
 
     if [[ -f /etc/systemd/system/find3server.service ]]; then
       echo -n "$(timestamp) [openHABian] Adding FIND3 to ZRAM... "
@@ -77,21 +76,21 @@ init_zram_mounts() {
     if ! openhab_is_installed; then
       echo -n "$(timestamp) [openHABian] Removing openHAB persistence from ZRAM... "
       if cond_redirect sed -i '/persistence.bind/d' /etc/ztab; then echo "OK"; else echo "FAILED (sed)"; return 1; fi
-    fi
-    if [[ -f $disklistFileDir ]]; then
-      echo -n "$(timestamp) [openHABian] Adding ZRAM to Amanda local backup... "
-      if ! cond_redirect sed -i '/zram/d' "$disklistFileDir"; then echo "FAILED (old config)"; return 1; fi
-      if (echo "${HOSTNAME}  /opt/zram/persistence.bind    comp-user-tar" >> "$disklistFileDir"); then echo "OK"; else echo "FAILED (new config)"; return 1; fi
-    fi
-    if [[ -f $disklistFileAWS ]]; then
-      echo -n "$(timestamp) [openHABian] Adding ZRAM to Amanda AWS backup... "
-      if ! cond_redirect sed -i '/zram/d' "$disklistFileAWS"; then echo "FAILED (old config)"; return 1; fi
-      if (echo "${HOSTNAME}  /opt/zram/persistence.bind    comp-user-tar" >> "$disklistFileAWS"); then echo "OK"; else echo "FAILED (new config)"; return 1; fi
+    else
+      if [[ -f $disklistFileDir ]]; then
+        echo -n "$(timestamp) [openHABian] Adding ZRAM to Amanda local backup... "
+        if ! cond_redirect sed -i '/zram/d' "$disklistFileDir"; then echo "FAILED (old config)"; return 1; fi
+        if (echo "${HOSTNAME}  /opt/zram/persistence.bind    comp-user-tar" >> "$disklistFileDir"); then echo "OK"; else echo "FAILED (new config)"; return 1; fi
+      fi
+      if [[ -f $disklistFileAWS ]]; then
+        echo -n "$(timestamp) [openHABian] Adding ZRAM to Amanda AWS backup... "
+        if ! cond_redirect sed -i '/zram/d' "$disklistFileAWS"; then echo "FAILED (old config)"; return 1; fi
+        if (echo "${HOSTNAME}  /opt/zram/persistence.bind    comp-user-tar" >> "$disklistFileAWS"); then echo "OK"; else echo "FAILED (new config)"; return 1; fi
+      fi
     fi
 
     echo -n "$(timestamp) [openHABian] Setting up ZRAM service... "
-    if ! sed -e "s|%STORAGE|${storageDir}|g" "$zramInstallLocation"/openhabian-zram/zram-config.service > /etc/systemd/system/zram-config.service; then echo "FAILED (install service)"; return 1; fi
-    if ! sed -e "s|%STORAGE|${storageDir}|g" "$zramInstallLocation"/openhabian-zram/zramsync.service > /etc/systemd/system/zramsync.service; then echo "FAILED (install service)"; return 1; fi
+    if ! sed -e "s|%STORAGE|${storageDir}|g" "$zramInstallLocation"/zram-config/zram-config.service > /etc/systemd/system/zram-config.service; then echo "FAILED (install service)"; return 1; fi
     if ! cond_redirect systemctl -q daemon-reload &> /dev/null; then echo "FAILED (daemon-reload)"; return 1; fi
 
     if ! cond_redirect install -m 644 "${BASEDIR:-/opt/openhabian}"/includes/sysctl-zram.conf /etc/sysctl.d/zram.conf; then echo "FAILED (add sysctl)"; return 1; fi
@@ -99,11 +98,11 @@ init_zram_mounts() {
       if ! cond_redirect sysctl -q -p /etc/sysctl.d/zram.conf ; then echo "FAILED (set sysctl parameters)"; return 1; fi
       if ! cond_redirect systemctl mask unattended-upgrades.service; then echo "FAILED (mask unattended upgrades service)"; return 1; fi
     fi
-    if cond_redirect systemctl enable --now zram-config.service zramsync.service; then echo "OK"; else echo "FAILED (enable service)"; return 1; fi
+    if cond_redirect systemctl enable --now zram-config.service; then echo "OK"; else echo "FAILED (enable service)"; return 1; fi
   elif [[ $1 == "uninstall" ]]; then
     echo -n "$(timestamp) [openHABian] Removing ZRAM service... "
-    if ! cond_redirect systemctl disable --now zram-config.service zramsync.service; then echo "FAILED (disable service)"; return 1; fi
-    if ! cond_redirect rm -f /etc/systemd/system/zram-config.service /etc/systemd/system/zramsync.service; then echo "FAILED (remove service)"; return 1; fi
+    if ! cond_redirect systemctl disable --now zram-config.service; then echo "FAILED (disable service)"; return 1; fi
+    if ! cond_redirect rm -f /etc/systemd/system/zram-config.service; then echo "FAILED (remove service)"; return 1; fi
     if ! running_in_docker && ! running_on_github; then
       if ! cond_redirect systemctl unmask unattended-upgrades.service; then echo "FAILED (unmask unattended upgrades service)"; return 1; fi
     fi
@@ -111,9 +110,7 @@ init_zram_mounts() {
 
     echo -n "$(timestamp) [openHABian] Removing ZRAM... "
     if ! cond_redirect rm -f /usr/local/sbin/zram-config; then echo "FAILED (zram-config)"; return 1; fi
-    if ! cond_redirect rm -f /usr/local/sbin/zramsync; then echo "FAILED (zramsync)"; return 1; fi
     if ! cond_redirect rm -f /etc/ztab; then echo "FAILED (ztab)"; return 1; fi
-    if ! cond_redirect rm -rf /storage/zram; then echo "FAILED (zramsync temp)"; return 1; fi
     if ! cond_redirect rm -rf /usr/local/share/zram-config; then echo "FAILED (zram-config share)"; return 1; fi
     if ! cond_redirect rm -rf /usr/local/lib/zram-config; then echo "FAILED (zram-config lib)"; return 1; fi
     if ! cond_redirect rm -rf /etc/sysctl.d/zram.conf; then echo "FAILED (sysctl.d/zram.conf)"; return 1; fi
