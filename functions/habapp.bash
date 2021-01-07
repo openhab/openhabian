9a8530c772cbb6e3d86cd096e17dc083c1065190
spacemanspiff2007 <10754716+spacemanspiff2007@users.noreply.github.com>
2021-01-07 05:49:14 +0100

    - Removed systemctl restart habapp.service - Added newline in habapp.service - Apply suggestions from code review
    Co-authored-by: Ethan Dye <mrtops03@gmail.com>
    Signed-off-by: Sebastian Gerber <github@sgerber.de>

--- functions/habapp.bash
+++ functions/habapp.bash
@@ -7,13 +7,13 @@
 ##    habapp_setup()
 ##
 habapp_setup() {
-  local installFolder=/opt
-  local venvName=habapp
-  local configFolder=/etc/openhab/habapp
+  local installFolder="/opt"
+  local venvName="habapp"
+  local configFolder="/etc/openhab/habapp"
 
   if [[ $1 == "remove" ]]; then
     echo -n "$(timestamp) [openHABian] Removing HABApp service ... "
-    if ! cond_redirect systemctl stop habapp.service; then echo "FAILED (stop service)"; return 1; fi
+    if ! cond_redirect systemctl disable --now habapp.service; then echo "FAILED (disable service)"; return 1; fi
     if ! rm -f /etc/systemd/system/habapp.service; then echo "FAILED (remove service)"; return 1; fi
     if cond_redirect systemctl -q daemon-reload &> /dev/null; then echo "OK"; else  echo "FAILED (daemon-reload)"; return 1; fi
 
@@ -43,18 +43,15 @@ habapp_setup() {
 
   echo -n "$(timestamp) [openHABian] Creating HABApp configuration folder ... "
   if ! cond_redirect mkdir -p "$configFolder"; then echo "FAILED"; return 1; fi
-  if ! cond_redirect chown -R openhab:openhabian /etc/openhab/habapp; then echo "FAILED"; return 1; fi
-  if cond_redirect chmod -R 775 /etc/openhab/habapp; then echo "OK"; else echo "FAILED"; return 1; fi
+  if ! cond_redirect chown -R openhab:openhabian "$configFolder"; then echo "FAILED"; return 1; fi
+  if cond_redirect chmod -R 775 "$configFolder"; then echo "OK"; else echo "FAILED"; return 1; fi
 
-  echo -n "$(timestamp) [openHABian] Setting up HABApp service ... "
+  echo -n "$(timestamp) [openHABian] Setting up HABApp as a service ... "
   if ! cond_redirect install -m 644 "${BASEDIR:-/opt/openhabian}"/includes/habapp.service /etc/systemd/system/habapp.service; then echo "FAILED (copy service)"; return 1; fi
   if ! cond_redirect systemctl -q daemon-reload &> /dev/null; then echo "FAILED (daemon-reload)"; return 1; fi
   if cond_redirect systemctl enable --now habapp.service; then echo "OK"; else echo "FAILED (enable service)"; return 1; fi
   
-  echo -n "$(timestamp) [openHABian] Starting HABApp ... "
-  if cond_redirect systemctl restart habapp.service; then echo "OK"; else echo "FAILED (starting service)"; return 1; fi
-  
   if [[ -n "$INTERACTIVE" ]]; then
-    whiptail --title "HABApp installed" --msgbox "HABApp was successfully installed on your system. A folder 'habapp' was created inside your openhab configuration folder." 8 80
+    whiptail --title "HABApp installed" --msgbox "HABApp was successfully installed on your system. A folder 'habapp' was created inside your openHAB configuration folder." 8 80
   fi
 }
