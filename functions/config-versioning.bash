549d96cbf65f6b0e16c165298c8711d2ea27a06e
James Bowler <denominator@gmail.com>
2021-01-22 07:08:05 +1000

    Typos and spelling fixes

--- functions/config-versioning.bash
+++ functions/config-versioning.bash
@@ -7,7 +7,11 @@
 ##
 versioning_setup() {
 
-  local user repositiory token logPrefix email
+  local user 
+  local email 
+  local repository 
+  local token 
+  local logPrefix="$(timestamp) [openHABian]"
   local introText="This is to create your own repository with you're config files for versioning and offsite storage. \\n\\nBefore setting up here you need to create an Github account, repository and access token.\\n\\nNOTE: This is not a full backup solution and will stop and restart openhab\\n\\n  Do you want to continue?"
   local installFound="Vesioning previously installed.\\n\\nUninstall will not delete online repository or local config files\\n\\nWould you like to Uninstall."
   local questionText="Please enter"
@@ -17,26 +21,25 @@ versioning_setup() {
   # shellcheck disable=SC2154
   email="$github_email"
   # shellcheck disable=SC2154
-  repositiory="$github_repositiory"
+  repository="$github_repository"
   # shellcheck disable=SC2154
   token="$github_token"
-  logPrefix="$(timestamp) [openHABian]"
 
   if [[ -n $UNATTENDED ]] 
   then
-    if [[ -z $github_token ]]
+    if [[ -z $github_token ]] # Check of credentials loaded from openhabaian.conf 
     then
-      echo "$logPrefix Github Versioning setup... CANCELED (Valid configuration not provided)"
+      echo "$logPrefix Github Versioning setup... CANCELED (No token found loaded)"
       return 0
     fi
   fi
 
-  echo -n "$logPrefix Check if Vesioning already exists..."
-  if [ -d /.git ]
+  echo -n "$logPrefix Check if vesioning already exists..."
+  if [ -d /.git ] # Make sure someone hasn't already init git on root folder
   then
     echo -n " Previous installation found"
     if [[ -n $INTERACTIVE ]];  then
-      if (whiptail --title "Versioning Github already installed" --yes-button "Uninstall" --no-button "Cancel" --yesno "$installFound" 14 80)
+      if (whiptail --title "Versioning github already installed" --yes-button "Uninstall" --no-button "Cancel" --yesno "$installFound" 14 80)
       then
         echo "Uninstall"  
         versioning_uninstall
@@ -47,7 +50,7 @@ versioning_setup() {
     fi
   fi
 
-  echo -n "$logPrefix Beginning the Github Repositiory Setup... "
+  echo -n "$logPrefix Beginning the github repository setup... "
   if [[ -n $INTERACTIVE ]]
   then 
     if (whiptail --title "Versioning Github" --yes-button "Continue" --no-button "Cancel" --yesno "$introText" 14 80)
@@ -64,28 +67,28 @@ versioning_setup() {
     if [ -z "$user" ]
     then
       if ! user="$(whiptail --title "Versioning Github" --inputbox "\\nGithub Username?" 8 39 --title "$questionText Username" 3>&1 1>&2 2>&3)"; then
-        echo "CANCELED UN"; return 0
+        echo "CANCELED (username)"; return 0
       fi
     fi
 
     if [ -z "$email" ]
     then
       if ! email="$(whiptail --title "Versioning Github" --inputbox "\\nGithub Email?" 8 39 --title "$questionText Email" 3>&1 1>&2 2>&3)"; then
-        echo "CANCELED UN"; return 0
+        echo "CANCELED (email)"; return 0
       fi
     fi
 
-    if [ -z "$repositiory" ] 
+    if [ -z "$repository" ] 
     then
-      if ! repositiory="$(whiptail --title "Versioning Github" --inputbox "\\nGithub Repository?" 8 39 --title "$questionText Repository" 3>&1 1>&2 2>&3)"; then
-        echo "CANCELED REPO"; return 0
+      if ! repository="$(whiptail --title "Versioning Github" --inputbox "\\nGithub Repository?" 8 39 --title "$questionText Repository" 3>&1 1>&2 2>&3)"; then
+        echo "CANCELED (repository"; return 0
       fi
     fi
 
     if [ -z "$token" ]
     then
       if ! token="$(whiptail --title "Versioning Github" --inputbox "\\nGithub token?" 8 39 --title "$questionText Token" 3>&1 1>&2 2>&3)"; then
-        echo "CANCELED Token"; return 0
+        echo "CANCELED (token)"; return 0
       fi
     fi
     echo " OK"
@@ -100,8 +103,8 @@ versioning_setup() {
     echo " OK"
   fi
   
-  repourl="https://$user:$token@github.com/$user/$repositiory.git"
-  echo -n "$logPrefix Check if $repositiory Exists and credentials valid..."
+  repourl="https://$user:$token@github.com/$user/$repository.git"
+  echo -n "$logPrefix Check if $repository Exists and credentials valid..."
   if git ls-remote "$repourl" | grep "HEAD" > /dev/null 
   then
     echo " OK"
@@ -109,26 +112,26 @@ versioning_setup() {
     echo " FAIL"
       if [[ -n $INTERACTIVE ]] 
       then 
-        whiptail --title "Operation Fail" --msgbox "Fail invalid credentials\\nYou entered\\n\\nUser         $user\\nEmail         $email\\nRepositiory  $repositiory\\nToken        $token" 15 80
+        whiptail --title "Operation Fail" --msgbox "Fail invalid credentials\\nYou entered\\n\\nUser         $user\\nEmail         $email\\nrepository  $repository\\nToken        $token" 15 80
       fi
     return 0
   fi
 
-  echo -n "$logPrefix Check if $repositiory is set to Private..."
-  if curl -s --head --request GET https://github.com/"$user"/"$repositiory" | grep "HTTP/2 404" > /dev/null
+  echo -n "$logPrefix Check if $repository is set to Private..."
+  if curl -s --head --request GET https://github.com/"$user"/"$repository" | grep "HTTP/2 404" > /dev/null
   then
     echo " OK"
   else
-    echo " FAIL"
+    echo " FAIL (Public repository)"
     if [[ -n $INTERACTIVE ]]
     then
-      whiptail --title "Openhabian Versioning" --msgbox "Please Set Repository [$repositiory] to Private on Github and try again" 8 78
+      whiptail --title "Openhabian Versioning" --msgbox "Please Set Repository [$repository] to Private on Github and try again" 8 78
       return 0
     fi
   fi
  
-  apiurl="https://api.github.com/repos/$user/$repositiory/contents/openhabian_config_versioning"
-  echo -n "$logPrefix Check online Repository [$repositiory] contains previous install..."
+  apiurl="https://api.github.com/repos/$user/$repository/contents/openhabian_config_versioning"
+  echo -n "$logPrefix Check online Repository [$repository] contains previous install..."
 
   if curl -s -H "Accept: application/vnd.github.v3+json" -i -u "$user":"$token" "$apiurl" | grep "HTTP/1.1 200 OK" > /dev/null
   then
@@ -154,7 +157,7 @@ versioning_setup() {
     echo "NO - Create new"
     cond_redirect git init / 
     git -C "/" remote add origin "$repourl"
-    echo "$logPrefix This file is intentionly left empty" > /openhabian_config_versioning
+    echo "$logPrefix This file is intentionally left empty" > /openhabian_config_versioning
     cp "${BASEDIR:-/opt/openhabian}/includes/cfg-gitignore.txt" /.gitignore
   fi
 
@@ -162,19 +165,19 @@ versioning_setup() {
   then 
     git -C "/" config user.user "$user"
   else 
-    exsisting_userename=$(git -c "/" config user.name)
-    echo "$logPrefix Git name already set [$exsisting_userename]... SKIPPED"
+    existing_userename=$(git -c "/" config user.name)
+    echo "$logPrefix git name already set [$existing_userename]... SKIPPED"
   fi
 
   if ! git -C "/" config user.email
   then 
     git -C "/" config user.email "$email"
   else 
-    exsisting_email=$(git -c "/" config user.email)
-    echo "$logPrefix Git email already set [$exsisting_email]... SKIPPED"
+    existing_email=$(git -c "/" config user.email)
+    echo "$logPrefix git email already set [$existing_email]... SKIPPED"
   fi
 
-  git -C "/" config versioning.repositiory "$repositiory"
+  git -C "/" config versioning.repository "$repository"
   git -C "/" config versioning.token "$token"
   cond_redirect git -C "/" push --set-upstream origin master
   git -C "/" config alias.cmp '!f() { git add -A && git commit -m "$@" && git push; }; f'
@@ -182,7 +185,7 @@ versioning_setup() {
   if [[ -n $INTERACTIVE ]]
   then
     systemctl start openhab.service
-    whiptail --title "Operation Successful!" --msgbox "Setup was successful.\\n\\nGithub repository with the name [$repositiory] is now up and running.\\nIt is ready for you to commit.\\n\\nYou can also enable auto commit daily" 15 80
+    whiptail --title "Operation Successful!" --msgbox "Setup was successful.\\n\\nGithub repository with the name [$repository] is now up and running.\\nIt is ready for you to commit.\\n\\nYou can also enable auto commit daily" 15 80
   fi
 
 }
@@ -201,7 +204,7 @@ versioning_uninstall() {
     (whiptail --title "Versioning Github" --yes-button "Continue" --no-button "Cancel" --yesno "No Versioning Found" 14 80) ; return 0
   fi
   currentrepo=$(cond_redirect sed -n -e "s/^.*\\(branch \\)/\\1/p" /.git/FETCH_HEAD)
-  if (whiptail --title "Versioning Github" --yes-button "Continue" --no-button "Cancel" --yesno "Found Existing Repositiory\\n\\n$currentrepo\\n\\nUninstall Versioning are you sure you want to continue" 14 80)
+  if (whiptail --title "Versioning Github" --yes-button "Continue" --no-button "Cancel" --yesno "Found Existing repository\\n\\n$currentrepo\\n\\nUninstall Versioning are you sure you want to continue" 14 80)
   then
     echo "OK"
   else
@@ -256,7 +259,7 @@ versioning_commit() {
 
   if [ -d "/.git" ]
   then 
-    echo "[openHABian] Start Repositiory commit... "
+    echo "[openHABian] Start repository commit... "
     example="$(date) Manual Commit"
     if [[ -n $INTERACTIVE ]];  then  commitMessage="$(whiptail --title "Versioning github commit message" --inputbox "\\nCommit Message" 8 80 "$example" 3>&1 1>&2 2>&3)"
       exitcode=$?
@@ -270,7 +273,7 @@ versioning_commit() {
       if [[ -n $INTERACTIVE ]];  then  whiptail --title "Versioning github" --scrolltext --msgbox "$commit" 25 80;  fi
     fi
   else
-    echo "[openHABian] No Repositiory Found to commit... "
+    echo "[openHABian] No repository Found to commit... "
     return 0
   fi
 
@@ -283,12 +286,12 @@ versioning_commit() {
 ##
 versioning_tokenupdate() {
 
-  local newtoken repositiory user newrepourl
+  local newtoken repository user newrepourl
 
   newtoken="$(whiptail --title "Versioning Github" --inputbox "\\nGithub Token?" 8 39 --title "Enter new Token" 3>&1 1>&2 2>&3)"
-  repositiory=$(git -C "/" config versioning.repositiory)
+  repository=$(git -C "/" config versioning.repository)
   user=$(git -C "/" config user.user)
-  newrepourl="https://$user:$newtoken@github.com/$user/$repositiory.git"
+  newrepourl="https://$user:$newtoken@github.com/$user/$repository.git"
   echo "$newrepourl"
   echo -n "$(timestamp) [openHABian] Check if new token is valid..."
   if git ls-remote "$newrepourl" | grep "HEAD" > /dev/null 
