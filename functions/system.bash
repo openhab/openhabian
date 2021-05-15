77eda368e451186005d980b3babe2b835603423e
Markus Storm <markus.storm@gmx.net>
2021-05-15 14:29:56 +0200

    Add Elelabs ZigBee controller chipset flash tool (#1520)
    * add Elelabs/Ember chipset ZigBee controller flash tool
    
    Signed-off-by: Markus Storm <markus.storm@gmx.net>

--- functions/system.bash
+++ functions/system.bash
@@ -559,3 +559,32 @@ prepare_serial_port() {
 
   whiptail --title "Operation successful" --msgbox "$successText" 9 80
 }
+
+
+## Function for installing the ELElabs EZSP firmware update utility
+## Useful to flash various ZigBee controllers that have an Ember chipset.
+##
+##    ezspUtility_setup
+##
+##
+ezspUtility_setup() {
+  local repo="https://github.com/Elelabs/elelabs-zigbee-ezsp-utility"
+  local emberfwrepo="https://github.com/xsp1989/zigbeeFirmware.git"     # for iTead (Sonoff) sticks
+  local destdir="/usr/share/openhab"
+  local target="Elelabs_EzspFwUtility.py"
+  local successText="The ELElabs python tool to flash Ember chipset ZigBee controllers has been successfully installed as ${destdir}/${target}.\\n\\nSee https://github.com/Elelabs/elelabs-zigbee-ezsp-utility#how-to how to use it."
+  local temp
+
+
+  temp="$(mktemp -d "${TMPDIR:-/tmp}"/openhabian.XXXXX)"
+
+  echo -n "$(timestamp) [openHABian] Installing ELElabs firmware flash tool ... "
+  cond_redirect git clone "$repo" "$temp"
+  if ! cond_redirect pip3 install -r ${temp}/requirements.txt; then echo "FAILED (install python requirements)"; return 1; fi
+  if cond_redirect install -o openhabian -g openhabian -m 755 $target $destdir; then echo "OK" else echo "FAILED (install .py)"; return 1; fi
+
+  if [[ -n $INTERACTIVE ]]; then
+    whiptail --title "Operation successful" --msgbox "$successText" 9 80
+  fi
+}
+
