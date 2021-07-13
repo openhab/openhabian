6ae8ec4d9e3b74a083326cb8076dfe5bb6c9dee3
Markus Storm <markus.storm@gmx.net>
2021-07-13 09:02:58 +0200

    Reset GPIO 18 on Homegear start to support HM-MOD-RPI-PCB controller (#1550)
    * reset GPIO 18 on homegear start to support HM-MOD-RPI-PCB controller
    
    Signed-off-by: Markus Storm <markus.storm@gmx.net>

--- functions/packages.bash
+++ functions/packages.bash
@@ -221,9 +221,10 @@ homegear_setup() {
   echo -n "$(timestamp) [openHABian] Setting up Homegear service... "
   if ! cond_redirect install -m 644 "${BASEDIR:-/opt/openhabian}"/includes/homegear.service /etc/systemd/system/homegear.service; then echo "FAILED (copy service)"; return 1; fi
   if ! cond_redirect install -m 644 "${BASEDIR:-/opt/openhabian}"/includes/homegear-management.service /etc/systemd/system/homegear-management.service; then echo "FAILED (copy service)"; return 1; fi
+  if ! cond_redirect install -m 755 "${BASEDIR}/includes/rpi_init" /usr/local/sbin; then echo "FAILED (install rpi_init script)"; return 1; fi
   if ! cond_redirect rm -f /lib/systemd/system/homegear*; then echo "FAILED (clean default service)"; return 1; fi
   if running_in_docker; then sed -i '/RuntimeDirectory/d' /etc/systemd/system/homegear*; fi
-  if ! zram_dependency install homegear homegear-management; then return 1; fi
+  if ! zram_dependency install homegear homegear-management wiringpi; then return 1; fi
   if [[ -s /etc/ztab ]] && ! mkdir -p /opt/zram/log.bind/homegear /var/log/homegear && chown homegear /opt/zram/log.bind/homegear /var/log/homegear; then echo "FAILED (create zram logdir)"; return 1; fi
   if ! cond_redirect systemctl enable --now homegear.service homegear-management.service; then echo "FAILED (enable service)"; return 1; fi
 
