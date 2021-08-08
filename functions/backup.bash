b108d35b999f5e7c20833dbc3aedf0d2d456679e
Markus Storm <markus.storm@gmx.net>
2021-08-08 11:29:07 +0200

    revert one hostname to HOSTNAME where no real need to change
    Signed-off-by: Markus Storm <markus.storm@gmx.net>

--- functions/backup.bash
+++ functions/backup.bash
@@ -161,7 +161,7 @@ create_amanda_config() {
   if ! cond_redirect mkdir -p "$configDir" "$databaseDir" "$logDir" "$indexDir"; then echo "FAILED (create directories)"; return 1; fi
   if ! cond_redirect touch "${configDir}/tapelist"; then echo "FAILED (touch tapelist)"; return 1; fi
   # shellcheck disable=SC2154
-  ip=$(dig +short "$hostname")
+  ip=$(dig +short "$HOSTNAME")
   revip=$(host "${ip}" | cut -d' ' -f1)
   if [[ -n "$revip" ]]; then (echo -e "${ip} ${backupUser} amdump\\n${revip} ${backupUser} amdump" > "$amandaHosts"); fi
   if ! (echo -e "${hostname} ${backupUser} amdump\\n${hostname} root amindexd amidxtaped\\nlocalhost ${backupUser}\\nlocalhost root amindexd amidxtaped" >> "$amandaHosts"); then echo "FAILED (Amanda hosts)"; return 1; fi
