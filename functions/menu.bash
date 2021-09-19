0ebf43f4b70213104a5adb8717120e4b2363e1dc
Markus Storm <markus.storm@gmx.net>
2021-09-19 13:58:38 +0200

    output excuted binary's name in menu
    Signed-off-by: Markus Storm <markus.storm@gmx.net>

--- functions/menu.bash
+++ functions/menu.bash
@@ -2,7 +2,7 @@
 
 show_about() {
   if openhab3_is_installed; then OHPKG="openhab"; else OHPKG="openhab2"; fi
-  whiptail --title "About openHABian and "$(basename "$0")" --msgbox "openHABian Configuration Tool — $(get_git_revision)
+  whiptail --title "About openHABian and $(basename "$0")" --msgbox "openHABian Configuration Tool — $(get_git_revision)
 openHAB $(sed -n 's/openhab-distro\s*: //p' /var/lib/${OHPKG}/etc/version.properties) - $(sed -n 's/build-no\s*: //p' /var/lib/${OHPKG}/etc/version.properties)
 \\nThis tool provides a little help to make your openHAB experience as comfortable as possible.
 Make sure you have read the README and know about the Debug and Backup guides in /opt/openhabian/docs.
