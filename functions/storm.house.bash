751e63d1f33659b844f42cf0207e0a9146efc40b
Markus Storm <markus.storm@gmx.net>
2021-10-12 20:28:01 +0200

    inverter aus UI installieren
    Signed-off-by: Markus Storm <markus.storm@gmx.net>

--- functions/storm.house.bash
+++ functions/storm.house.bash
@@ -303,6 +303,8 @@ setup_wb_config() {
     if [[ -z "${1:-$wallboxtype}" ]]; then echo "SKIPPED (no wallbox defined)"; return 1; fi
   fi
 
+   if ! cond_redirect install -m 755 "${sdIncludesDir}/setup_inverter" /usr/local/sbin; then echo "FAILED (install setup_inverter)"; return 1; fi
+
   for component in things items rules; do
     rm -f "${OPENHAB_CONF:-/etc/openhab}/${component}/wb.${component}"
     srcfile="${OPENHAB_CONF:-/etc/openhab}/${component}/STORE/${1:-${wallboxtype}}.${component}"
