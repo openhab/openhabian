4eb12b231c7f27010999deb06bc57d56e3cdf6dd
Markus Storm <markus.storm@gmx.net>
2021-10-18 18:50:03 +0200

    cosmetics
    Signed-off-by: Markus Storm <markus.storm@gmx.net>

--- functions/vpn.bash
+++ functions/vpn.bash
@@ -239,7 +239,7 @@ setup_tailscale() {
     return 0
   fi
   if [[ -n "$INTERACTIVE" ]]; then
-    if ! preAuthKey="$(whiptail --title "Enter pre auth key" --inputbox "\\nIf you have not received / created the tailscale pre auth key at this stage, please do so now or tell your administrator to. This can be done on the admin console. There's a menu option on the tailscale Windows client to lead you there.\\n\\nPlease enter the tailscale pre auth key for this system:" 11 80 "$preAuthKey" 3>&1 1>&2 2>&3)"; then echo "CANCELED"; return 0; fi
+    if ! preAuthKey="$(whiptail --title "Enter pre auth key" --inputbox "\\nIf you have not received / created the tailscale pre auth key at this stage, please do so now or tell your administrator to. This can be done on the admin console. There's a menu option on the tailscale Windows client to lead you there.\\n\\nPlease enter the tailscale pre auth key for this system:" 12 80 "$preAuthKey" 3>&1 1>&2 2>&3)"; then echo "CANCELED"; return 0; fi
   fi
 
   # if ${tags}/${tstags} is empty, this will reset existing tags
