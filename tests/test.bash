df324920febf2af5ba762698099c10919f194b48
Ethan Dye <mrtops03@gmail.com>
2021-11-29 21:50:15 -0700

    Fix path to `ci-setup.bash` in `test.bash`
    Signed-off-by: Ethan Dye <mrtops03@gmail.com>

--- tests/test.bash
+++ tests/test.bash
@@ -18,7 +18,7 @@ if [[ $1 == "shellcheck" ]]; then
   shellcheck -x -s bash openhabian-setup.sh
   shellcheck -x -s bash functions/*.bash
   shellcheck -x -s bash build-image/*.bash
-  shellcheck -x -s bash build.bash ci-setup.bash
+  shellcheck -x -s bash build.bash tests/ci-setup.bash
 else
   echo_process "Please provide a valid test profile, \"shellcheck\". Exiting"
   exit 0
