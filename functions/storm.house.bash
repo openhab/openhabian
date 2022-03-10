ec549cf23759f495a943bf2c48a1175dfa659e6a
Markus Storm <markus.storm@gmx.net>
2022-03-10 11:09:46 +0100

    fix inverter var in png copying
    Signed-off-by: Markus Storm <markus.storm@gmx.net>

--- functions/storm.house.bash
+++ functions/storm.house.bash
@@ -325,7 +325,7 @@ setup_wb_config() {
       fi
     fi
   done
-  cp "${OPENHAB_CONF:-/etc/openhab}/icons/STORE/${invertertype}.png" /etc/openhab/icons/inverter.png
+  cp "${OPENHAB_CONF:-/etc/openhab}/icons/STORE/${1:-${invertertype}}.png" /etc/openhab/icons/inverter.png
 
   srcfile="${OPENHAB_CONF:-/etc/openhab}/icons/STORE/wallbox/${1:-${wallboxtype}}.png"
   if [[ -f $srcfile ]]; then
