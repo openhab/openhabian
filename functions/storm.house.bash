3504710328e5f8cb2b6f4810e91de90f4845f642
Markus Storm <markus.storm@gmx.net>
2022-03-10 11:17:46 +0100

    fix png permissions
    Signed-off-by: Markus Storm <markus.storm@gmx.net>

--- functions/storm.house.bash
+++ functions/storm.house.bash
@@ -326,6 +326,10 @@ setup_wb_config() {
     fi
   done
   cp "${OPENHAB_CONF:-/etc/openhab}/icons/STORE/${1:-${invertertype}}.png" /etc/openhab/icons/inverter.png
+  if [[ $(whoami) == "root" ]]; then
+    chown "${username:-openhabian}:openhab" "${OPENHAB_CONF:-/etc/openhab}/icons/inverter.png
+    chmod 664 "${OPENHAB_CONF:-/etc/openhab}/icons/inverter.png"
+  fi
 
   srcfile="${OPENHAB_CONF:-/etc/openhab}/icons/STORE/wallbox/${1:-${wallboxtype}}.png"
   if [[ -f $srcfile ]]; then
