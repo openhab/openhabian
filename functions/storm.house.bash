e0f83cca109965e66cd91ff1944039feeae1e4f4
Markus Storm <markus.storm@gmx.net>
2022-04-22 19:47:27 +0200

    LadeleistungHaus
    Signed-off-by: Markus Storm <markus.storm@gmx.net>

--- functions/storm.house.bash
+++ functions/storm.house.bash
@@ -274,7 +274,16 @@ setup_wb_config() {
 ## Valid Arguments: manuellWallbox | openwb
 ##                  IP address of wallbox
 ##
-##    setup_wb_config(String wallbox type,String wallbox IP)
+## * wallbox type (from EVCC)
+## abl cfos easee eebus evsewifi go-e heidelberg keba mcc nrgkick-bluetooth nrgkick-connect
+## openwb phoenix-em-eth phoenix-ev-eth phoenix-ev-ser simpleevse wallbe warp
+## * IP address of wallbox
+## * car type (from EVCC)
+## audi bmw carwings citroen ds opel peugeot fiat ford kia hyundai mini nissan niu tesla
+## renault ovms porsche seat skoda enyaq vw id volvo tronity
+## * car name
+##
+##    setup_wb_config(String wallbox typ,String wallbox IP,String auto Ttyp,String autoname)
 ##
 setup_wb_config() {
   local includesDir="${BASEDIR:-/opt/openhabian}/includes"
@@ -282,26 +291,27 @@ setup_wb_config() {
 
   if [[ -n "$UNATTENDED" ]]; then
     echo -n "$(timestamp) [storm.house] wallbox installation... "
-    if [[ -z "${1:-$wallboxtype}" ]]; then echo "SKIPPED (no wallbox defined)"; return 1; fi
+    if [[ -z "${1:-$wallboxtyp}" ]]; then echo "SKIPPED (no wallbox defined)"; return 1; fi
   fi
 
   if [[ -n "$INTERACTIVE" ]]; then
-    if [[ -z "${1:-$wallboxtype}" ]]; then
-        if ! wallboxtype="$(whiptail --title "Wallbox Auswahl" --cancel-button Cancel --ok-button Select --menu "\\nWählen Sie den Wallboxtyp aus" 12 80 0 "openwb" "openWB" "goe" "go-E Charger" "myenergi" "myEnergi Zappi" "keba" "KeContact P20/P30" "wbmanuell" "keine (manuelle Konfiguration)" 3>&1 1>&2 2>&3)"; then unset wallboxtype; return 1; fi
+    if [[ -z "${1:-$wallboxtyp}" ]]; then
+      if ! wallboxtyp="$(whiptail --title "Wallbox Auswahl" --cancel-button Cancel --ok-button Select --menu "\\nWählen Sie den Wallboxtyp aus" 12 80 0 "abl" "ABL eMH1" "cfos" "cFos PowerBrain charger" "easee" "Easee Home Wallbox" "evsewifi" "Wallboxen mit SimpleEVSE Controller" "go-e" "go-E Charger" "heidelberg" "Heidelberg Energy Control" "keba" "KEBA KeContact P20/P30 und BMW Wallboxen" "mcc" "Mobile Charger Connect (Audi, Bentley Porsche)" "nrgkick-bluetooth" "NRGkick Wallbox mit Bluetooth" "nrgkick-connect" "NRGkick Wallbox mit zusätzlichem NRGkick Connect Modul" "openwb" "openWB Wallbox via MQTT" "phoenix-em-eth" "Wallboxen mit dem Phoenix EM-CP-PP-ETH Controller" "phoenix-ev-eth" "Wallboxen mit dem Phoenix EV-CC-**-ETH Controller" "phoenix-ev-ser" "Wallboxen mit dem Phoenix EV-CC-***-SER seriellen Controller" "simpleevse" "Wallboxen mit dem SimpleEVSE Controller" "wallbe" "Wallbe Eco Wallbox" "warp" "Tinkerforge Warp/Warp Pro" "custom" "manuelle Konfiguration" 3>&1 1>&2 2>&3)"; then unset wallboxtyp wallboxip autotyp autoname; return 1; fi
     fi
-    if ! wallboxip=$(whiptail --title "Wallbox IP" --inputbox "Welche IP-Adresse hat die Wallbox ?" 10 60 "${wallboxip:-192.168.178.200}" 3>&1 1>&2 2>&3); then unset wallboxtype wallboxip; return 1; fi
+    if ! wallboxip=$(whiptail --title "Wallbox IP" --inputbox "Welche IP-Adresse hat die Wallbox ?" 10 60 "${wallboxip:-192.168.178.200}" 3>&1 1>&2 2>&3); then unset wallboxtyp wallboxip autotyp autoname; return 1; fi
+    if ! autotyp="$(whiptail --title "Auswahl Autohersteller" --cancel-button Cancel --ok-button Select --menu "\\nWählen Sie den Hersteller Ihres Fahrzeugs aus" 12 80 0 "audi" "Audi" "bmw" "BMW" "carwings" "Nissan z.B. Leaf vor 2019" "citroen" "Citroen" "ds" "DS" "opel" "Opel" "peugeot" "Peugeot" "fiat" "Fiat, Alfa Romeo" "ford" "Ford" "kia" "Kia Motors" "hyundai" "Hyundai" "mini" "Mini" "nissan" "neue Nissan Modelle ab 2019" "niu" "NIU" "tesla" "Tesla Motors" "renault" "Renault, Dacia" "ovms" "Open Vehicle Monitoring System" "porsche" "Porsche" "seat" "Seat" "skoda" "Skoda Auto" "enyaq" "Skoda Enyac" "vw" "Volkswagen ausser ID-Modelle" "id" "Volkswagen ID-Modelle" "volvo" "Volvo" "tronity" "Fahrzeuge über Tronity" "custom" "manuelle Konfiguration" 3>&1 1>&2 2>&3)"; then unset wallboxtyp wallboxip autotyp autoname; return 1; fi
+    if ! autoname=$(whiptail --title "Auto Modell" --inputbox "Automodell" 10 60 "${autoname:-tesla}" 3>&1 1>&2 2>&3); then unset wallboxtyp wallboxip autotyp autoname; return 1; fi
   fi
 
   if [[ ! -f /usr/local/sbin/setup_wb_config ]]; then
     if ! cond_redirect ln -s "${includesDir}/setup_ems_hw" /usr/local/sbin/setup_wb_config; then echo "FAILED (install setup_wb_config script)"; return 1; fi
-    #if ! cond_redirect install -m 755 "${includesDir}/setup_ems_hw" /usr/local/sbin/setup_wb_config; then echo "FAILED (install setup_wb_config script)"; return 1; fi
   fi
 
   for component in things items rules; do
-    if [[ ${1:-${wallboxtype}} == "none" ]]; then
+    if [[ ${1:-${wallboxtyp}} == "none" ]]; then
       rm -f "${OPENHAB_CONF:-/etc/openhab}/${component}/wb.${component}"
     else
-      cp "${OPENHAB_CONF:-/etc/openhab}/${component}/STORE/${1:-${wallboxtype}}.${component}" "${OPENHAB_CONF:-/etc/openhab}/${component}/wb.${component}"
+      cp "${OPENHAB_CONF:-/etc/openhab}/${component}/STORE/${1:-${wallboxtyp}}.${component}" "${OPENHAB_CONF:-/etc/openhab}/${component}/wb.${component}"
       if [[ $(whoami) == "root" ]]; then
         chown "${username:-openhabian}:openhab" "${OPENHAB_CONF:-/etc/openhab}/${component}/wb.${component}"
         chmod 664 "${OPENHAB_CONF:-/etc/openhab}/${component}/wb.${component}"
@@ -309,12 +319,14 @@ setup_wb_config() {
     fi
   done
 
+  sed -i "s|%WBTYP|${1:-${wallboxtyp}}|" "${OPENHAB_CONF:-/etc/openhab}/things/wb.things"
   sed -i "s|%IP|${2:-${wallboxip}}|" "${OPENHAB_CONF:-/etc/openhab}/things/wb.things"
+  sed -i "s|%AUTOTYP|${3:-${autotyp}}|" "${OPENHAB_CONF:-/etc/openhab}/things/wb.things"
+  sed -i "s|%AUTONAME|${3:-${autoname}}|" "${OPENHAB_CONF:-/etc/openhab}/things/wb.things"
   
   echo "OK"
   if [[ -n "$INTERACTIVE" ]]; then
     whiptail --title "Installation erfolgreich" --msgbox "Das Energie Management System nutzt jetzt eine ${1:-${wallboxtype}} Wallbox." 8 80
->>>>>>> c95c9ddc (Wallbox in Einstellungen auswaehlbar machen)
   fi
 }
 
@@ -435,6 +447,9 @@ activate_ems() {
     echo "Falsche Lizenz, deaktiviere ..."
     curl -X POST --header "Content-Type: text/plain" --header "Accept: application/json" -d "KEINE" "http://${hostname}:8080/rest/items/LizenzStatus"
     # TODO: eventuell systemd timer setzen der in 1 Monat modbus disabled
+=======
+    whiptail --title "Installation erfolgreich" --msgbox "Das Energie Management System nutzt jetzt eine ${1:-${wallboxtyp}} Wallbox mit einem ${3:-${autotyp}}." 8 80
+>>>>>>> 988aa4e3 (LadeleistungHaus)
   fi
 }
 
