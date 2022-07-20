bfd0a4133e624b806dccf7287600c22d00e8af04
openhabian <openhabian@openHABian>
2022-07-20 13:47:33 +0200

    menu 03 -> update EMS
    Signed-off-by: openhabian <openhabian@openHABian>

--- functions/menu.bash
+++ functions/menu.bash
@@ -84,9 +84,6 @@ show_main_menu() {
     setup_pv_config
     setup_wb_config
 
-  elif [[ "$choice" == "06"* ]]; then
-    update_ems
-
   elif [[ "$choice" == "10"* ]]; then
     choice2=$(whiptail --title "storm.house Configuration Tool $(get_git_revision)" --menu "Apply Improvements" 13 116 6 --cancel-button Back --ok-button Execute \
     "11 | Packages"               "Install needed and recommended system packages" \
