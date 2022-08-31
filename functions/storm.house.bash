418fd21a85e7f23d8ff2b998b892762bb7678d95
openhabian <openhabian@openHABian>
2022-08-31 17:43:07 +0200

    install extra solar forecast jar
    Signed-off-by: openhabian <openhabian@openHABian>

--- functions/storm.house.bash
+++ functions/storm.house.bash
@@ -576,9 +576,9 @@ retrieve_license() {
 
 ## Install non-standard bindings etc
 ##
-##    install_extras()
+##    install_openhab_extras()
 ##
-install_extras() {
+install_openhab_extras() {
   local jar=org.openhab.binding.solarforecast-3.4.0-SNAPSHOT.jar
   local pkg="https://github.com/weymann/OH3-SolarForecast-Drops/blob/main/3.4/${jar}"
   local dest="/usr/share/openhab/addons/${jar}"
