c075f3bde6e6cfda2bbefb6066ca39658ce283b0
openhabian <openhabian@openHABian>
2022-10-06 17:48:01 +0200

    fix sed try 2
    Signed-off-by: openhabian <openhabian@openHABian>

--- functions/storm.house.bash
+++ functions/storm.house.bash
@@ -58,14 +58,12 @@ setup_pv_config() {
           chmod 664 "${OPENHAB_CONF:-/etc/openhab}/${configdomain}/${device}.${configdomain}"
         fi
       fi
-    done
 
-    # TODO .things, .items, (.rules auch?) in pv/ bat/ meter/ aufteilen
-    # %MBID einbauen
-    sed -i "s|%IP|${3:-${ip}}|;s|%MBID|${4:-${mbid}}|" "${OPENHAB_CONF:-/etc/openhab}/things/${device}.things"
-    #if [[ $# -gt 4 ]]; then
-    #    sed -i "s|%METERIP|${3:-${meterip}}|" "${OPENHAB_CONF:-/etc/openhab}/things/${device}.things"
-    #fi
+      sed -i "s|%IP|${3:-${ip}}|;s|%MBID|${4:-${mbid}}|" "${OPENHAB_CONF:-/etc/openhab}/things/${device}.things"
+      #if [[ $# -gt 4 ]]; then
+      #    sed -i "s|%METERIP|${3:-${meterip}}|" "${OPENHAB_CONF:-/etc/openhab}/things/${device}.things"
+      #fi
+    done
   done
 
 
