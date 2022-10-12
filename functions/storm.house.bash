c412f2bc3c3d0cfff1da548925cf539eb51e398d
openhabian <openhabian@openHABian>
2022-10-12 17:07:56 +0200

    wb default wbcustom
    Signed-off-by: openhabian <openhabian@openHABian>

--- functions/storm.house.bash
+++ functions/storm.house.bash
@@ -427,9 +427,6 @@ setup_wb_config() {
 
   cp "${includesDir}/EVCC/evcc.yaml-template" "$evcccfg"
   sed -i "s|%WBTYP|${1:-${wallboxtype}}|;s|%IP|${2:-${wallboxip}}|;s|%AUTOTYP|${3:-${autotyp}}|" "$evcccfg"
-  #sed -i "s|%WBTYP|${1:-${wallboxtype}}|" "$evcccfg"
-  #sed -i "s|%IP|${2:-${wallboxip}}|" "$evcccfg"
-  #sed -i "s|%AUTOTYP|${3:-${autotyp}}|" "$evcccfg"
   
   echo "OK"
   if [[ -n "$INTERACTIVE" ]]; then
