4c380bf4109e91d3d84a30f3f0733051991171b8
Markus Storm <markus.storm@gmx.net>
2022-10-14 20:26:45 +0200

    fix ssh key permissions if exists
    Signed-off-by: Markus Storm <markus.storm@gmx.net>

--- functions/system.bash
+++ functions/system.bash
@@ -291,6 +291,7 @@ permissions_corrections() {
   local openhabFolders=("/etc/openhab" "/var/lib/openhab" "/var/log/openhab" "/usr/share/openhab")
   local openhabHome="/var/lib/openhab"
   local backupsFolder="${OPENHAB_BACKUPS:-/var/lib/openhab/backups}"
+  local privkey=/var/lib/openhab/etc/openhab_rsa
   local retval=0
 
   echo -n "$(timestamp) [openHABian] Applying file permissions recommendations... "
@@ -382,6 +383,9 @@ permissions_corrections() {
     if cond_redirect fix_permissions "/opt/habapp" 775 775; then echo "OK"; else echo "FAILED (HABApp venv permissions)"; retval=1; fi
   fi
 
+  if [[ -f "${privkey}" ]]; then
+     chmod 600 "${privkey}"
+  fi
   return $retval
 }
 
