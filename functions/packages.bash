3ea8b6fe3442ec2e16f081827f1b999b7bf0f265
Markus Storm <markus.storm@gmx.net>
2023-01-06 13:35:40 +0100

    Update packages.bash
    Signed-off-by: Markus Storm <markus.storm@gmx.net>

--- functions/packages.bash
+++ functions/packages.bash
@@ -758,6 +758,7 @@ setup_evcc() {
     cond_redirect cp /home/"${username:-openhabian}"/${evccConfig} /home/"${username:-openhabian}"/${evccConfig}.SAVE
     cond_redirect mv "${evccConfig}" /home/"${username:-openhabian}"
     cond_redirect chown "${username}" /home/"${username:-openhabian}"/${evccConfig}*
+    cond_redirect chmod g+w /home/"${username:-openhabian}"/${evccConfig}*
   fi
   echo -n "$(timestamp) [openHABian] Restarting EVCC... "
   if cond_redirect systemctl restart evcc.service; then echo "OK"; else echo "FAILED"; fi
