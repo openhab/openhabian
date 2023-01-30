1b8439a1fe8ed8fde88b7363c4726743a72f7640
openhabian <openhabian@openHABian>
2023-01-30 17:56:39 +0100

    add final OH restart
    Signed-off-by: openhabian <openhabian@openHABian>

--- openhabian-setup.sh
+++ openhabian-setup.sh
@@ -131,6 +131,7 @@ if [[ -n "$UNATTENDED" ]]; then
   setup_pv_config meter "${metertype:-inverter}" "${meterip:-192.168.178.102}" "${metermodbusid:-99}"
   setup_wb_config "${wallboxtype:-openwb-pro}" "${wallboxip:-192.168.178.200}"
   install_cleanup
+  systemctl restart openhab
 else
   apt_update
   whiptail_check
