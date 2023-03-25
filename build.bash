1f454a50f4c8b05c67b0dbd7d6dae9000509bf9f
Markus Storm <markus.storm@gmx.net>
2023-03-26 00:21:22 +0100

    build can now only use openhabian.conf, too
    Signed-off-by: Markus Storm <markus.storm@gmx.net>

--- build.bash
+++ build.bash
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-# shellcheck disable=SC2016
-# shellcheck disable=SC1091
+# shellcheck disable=SC2016,SC1090,SC1091
 set -e
 
 ####################################################################
