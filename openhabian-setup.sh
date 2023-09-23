f5c7f8a7b5c5ff59de3df922443234d8b8f8147f
openhabian <openhabian@openHABian>
2023-09-23 10:37:15 +0200

    Samba wieder an; EVCC 0.120.1
    Signed-off-by: openhabian <openhabian@openHABian>

--- openhabian-setup.sh
+++ openhabian-setup.sh
@@ -119,12 +119,12 @@ if [[ -n "$UNATTENDED" ]]; then
   nano_openhab_syntax
   multitail_openhab_scheme
   #srv_bind_mounts
-  #samba_setup
+  samba_setup
   clean_config_userpw
   install_grott "install"
   permissions_corrections
   setup_mirror_SD "install"
-  install_evcc "install" "${evccpkgversion:-0.119.1}"; setup_evcc
+  install_evcc "install" "${evccpkgversion:-0.120.1}"; setup_evcc
   #sleep 30
   systemctl stop openhab
   setup_pv_config pv "${invertertype:-custom}" "${inverterip:-192.168.178.100}" "${invertermodbusid:-1}"
