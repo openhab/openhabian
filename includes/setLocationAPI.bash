1be2effb8ae94fd2ac56f2bb8ed8723214d9aa49
openhabian <openhabian@openHABian>
2023-11-14 22:45:46 +0100

    Komma an falscher Stelle führt zu nicht-syntaktisch korrektem astro.things
    Signed-off-by: openhabian <openhabian@openHABian>

--- includes/setLocationAPI.bash
+++ includes/setLocationAPI.bash
@@ -2,5 +2,5 @@
 
 /usr/bin/echo "org.openhab.i18n:location=${1}" > /etc/openhab/services/location.cfg
 sed -i "s|location=.*|location=\"${1}\"|;s|apikey=.*|apikey=\"${2}\",|" "${OPENHAB_CONF:-/etc/openhab}/things/wetter.things"
-sed -i "s|geolocation=.*|geolocation=\"${1},\"|" "${OPENHAB_CONF:-/etc/openhab}/things/astro.things"
+sed -i "s|geolocation=.*|geolocation=\"${1}\",|" "${OPENHAB_CONF:-/etc/openhab}/things/astro.things"
 
