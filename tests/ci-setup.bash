dfe2bbe9987c6074ecb268944c9241f01734ed76
Markus Storm <markus.storm@gmx.net>
2023-11-21 16:11:33 +0100

    variable clonebranch
    Signed-off-by: Markus Storm <markus.storm@gmx.net>

--- tests/ci-setup.bash
+++ tests/ci-setup.bash
@@ -35,7 +35,7 @@ if [[ $1 == "github" ]]; then
 
   sed -i 's|repositoryurl=.*$|repositoryurl='"${repoURL}"'|' build-image/*.conf
   if git show-ref --tags "$2" --quiet; then
-    sed -i 's|clonebranch=.*$|clonebranch=openHAB|' build-image/*.conf
+    sed -i 's|clonebranch=.*$|clonebranch='"${clonebranch:-openHAB}"'|' build-image/*.conf
     exit
   fi
   sed -i 's|clonebranch=.*$|clonebranch='"${repoBranch}"'|' build-image/*.conf
