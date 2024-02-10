12f85cb8472744a816d603c8330820b2572a1a25
Markus Storm <markus.storm@gmx.net>
2024-02-11 00:22:47 +0100

    install npm if missing
    Signed-off-by: Markus Storm <markus.storm@gmx.net>

--- functions/nodejs-apps.bash
+++ functions/nodejs-apps.bash
@@ -46,7 +46,7 @@ nodejs_setup() {
       if cond_redirect apt-get install --yes -o DPkg::Lock::Timeout="$APTTIMEOUT" nodejs; then echo "OK"; else echo "FAILED"; return 1; fi
     fi
     #if [[ "$myDistro" == "bookworm" ]]; then jsscripting_npm_install; fi
-    if ! npm -v; then jsscripting_npm_install; fi
+    if ! command -v npm; &> /dev/null; then jsscripting_npm_install; fi
   fi
 }
 
