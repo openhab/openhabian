4182cc80d64ce280197567a601cc871633d494e3
Markus Storm <markus.storm@gmx.net>
2024-03-11 10:19:02 +0100

    fix homegear testing repo
    Signed-off-by: Markus Storm <markus.storm@gmx.net>

--- functions/packages.bash
+++ functions/packages.bash
@@ -205,9 +205,12 @@ homegear_setup() {
   if [[ "$myRelease" == "n/a" ]]; then
     myRelease="${osrelease:-bookworm}"
   fi
-  if [[ "$myOS" == "Raspbian" ]] || is_arm && running_in_docker; then  # Workaround for CI not actually reporting as Raspberry Pi OS
+  #if [[ "$myOS" == "Raspbian" ]] || is_arm && running_in_docker; then  # Workaround for CI not actually reporting as Raspberry Pi OS
+  if [[ "$myOS" == "debian" ]]
+    if [[ is_arm || running_in_docker ]]; then  # Workaround for CI not actually reporting as Raspberry Pi OS
     #myOS="debian"  # Workaround for Homegear's Raspios APT repo being broken
-    myOS="raspberry_pi_os"  # Workaround for Homegear's Raspios APT repo being broken
+      myOS="raspberry_pi_os"  # Workaround for Homegear's Raspios APT repo being broken
+    fi
   fi
 
   echo -n "$(timestamp) [openHABian] Beginning Homematic CCU2 emulation software Homegear install... "
