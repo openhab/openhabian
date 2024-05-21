e9e8776c642ed2cf9ea68bab80ba9af2d7f8948f
openhabian <openhabian@openHABian>
2024-05-21 19:41:28 +0200

    setup_forecastsolar
    Signed-off-by: openhabian <openhabian@openHABian>

--- includes/setLocationAPI.bash
+++ includes/setLocationAPI.bash
@@ -2,5 +2,6 @@
 
 /usr/bin/echo "org.openhab.i18n:location=${1}" > /etc/openhab/services/location.cfg
 sed -i "s|location=.*|location=\"${1}\"|;s|apikey=.*|apikey=\"${2}\",|" "${OPENHAB_CONF:-/etc/openhab}/things/wetter.things"
+sed -i "s|location=.*|location=\"${1}\"|" "${OPENHAB_CONF:-/etc/openhab}/things/forecastsolar.things"
 sed -i "s|geolocation=.*|geolocation=\"${1}\",|" "${OPENHAB_CONF:-/etc/openhab}/things/astro.things"
 
