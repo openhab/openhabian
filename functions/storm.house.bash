5a9ee4a1060fba62a9b9c574d9ae9c4c9c842f37
openhabian <openhabian@openHABian>
2024-09-27 16:17:12 +0200

    WP konfig.barer Port hinzugefügt
    Signed-off-by: openhabian <openhabian@openHABian>

--- functions/storm.house.bash
+++ functions/storm.house.bash
@@ -386,12 +386,12 @@ setup_hp_config() {
     fi
   done
 
-  muser=${5:-${heatpumpuser}}
+  muser=${6:-${heatpumpuser}}
   if [[ $muser == "NULL" ]]; then muser=""; fi
-  mpass=${6:-${heatpumppass}}
+  mpass=${7:-${heatpumppass}}
   if [[ $mpass == "NULL" ]]; then mpass=""; fi
-  #sed -i "s|%IP|${2:-${heatpumpip}}|;s|%MBID|${3:-${heatpumpmodbusid:-1}}|;s|%RELAY|${4:-${sgractuator:-shelly25-relay}}|;s|%USER|${5:-${heatpumpuser}}|;s|%PASS|${6:-${heatpumppass}}|" "${OPENHAB_CONF:-/etc/openhab}/things/heizung.things"
-  sed -i "s|%IP|${2:-${heatpumpip}}|;s|%MBID|${3:-${heatpumpmodbusid:-1}}|;s|%RELAY|${4:-${sgractuator:-shelly25-relay}}|;s|%USER|${muser}|;s|%PASS|${mpass}|" "${OPENHAB_CONF:-/etc/openhab}/things/heizung.things"
+  #sed -i "s|%IP|${2:-${heatpumpip}}|;s|%MBID|${3:-${heatpumpmodbusid:-1}}|;s|%RELAY|${4:-${sgractuator:-shelly25-relay}}|;s|%USER|${muser}|;s|%PASS|${mpass}|" "${OPENHAB_CONF:-/etc/openhab}/things/heizung.things"
+  sed -i "s|%IP|${2:-${heatpumpip}}|;s|%PORT|${3:-${heatpumpport:-8889}}|;s|%MBID|${4:-${heatpumpmodbusid:-1}}|;s|%RELAY|${5:-${sgractuator:-shelly25-relay}}|;s|%USER|${muser}|;s|%PASS|${mpass}|" "${OPENHAB_CONF:-/etc/openhab}/things/heizung.things"
   sed -i "s|%RELAY|${4:-${sgractuator:-shelly25-relay}}|" "${OPENHAB_CONF:-/etc/openhab}/items/heizung.items"
 }
 
