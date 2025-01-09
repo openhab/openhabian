f694a3ac606cc3257554813842e731fd11eb23fa
Carsten Mogge <carsten.mogge@gmail.com>
2025-01-09 20:35:05 +0100

    Update functions/nodejs-apps.bash
    Co-authored-by: Ethan Dye <mrtops03@gmail.com>
    Signed-off-by: Carsten Mogge <carsten.mogge@gmail.com>

--- functions/nodejs-apps.bash
+++ functions/nodejs-apps.bash
@@ -317,9 +317,9 @@ zigbee2mqtt_setup() {
     if ! cond_redirect cd /opt/zigbee2mqtt; then echo "FAILED (cd zigbee2mqtt)"; return 1; fi
     if ! cond_redirect systemctl stop zigbee2mqtt ; then echo "FAILED (stop systemctl)"; fi
     if ! cond_redirect sudo -u "${username:-openhabian}" cp -R data data-backup; then echo "FAILED (cp backup)"; return 1; fi
-    if ! cond_redirect sudo -u "${username:-openhabian}" git checkout 1.42.0; then echo "FAILED git checkout"; return 1; fi
     if ! cond_redirect sudo -u "${username:-openhabian}" git fetch origin; then echo "FAILED git fetch"; return 1; fi
     if ! cond_redirect sudo -u "${username:-openhabian}" git fetch --tags; then echo "FAILED git fetch"; return 1; fi
+    if ! cond_redirect sudo -u "${username:-openhabian}" git checkout 1.42.0; then echo "FAILED git checkout"; return 1; fi
 
     if ! cond_redirect sudo -u "${username:-openhabian}" npm ci; then echo "FAILED npm"; return 1; fi
     if ! cond_redirect sudo -u "${username:-openhabian}" cp -R data-backup/* data; then echo "FAILED (cp backup)"; return 1; fi
