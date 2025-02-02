818ed0fc2fd80a710bda7c554e2f9f1f3f600dfb
openhabian <openhabian@openHABian>
2025-02-02 17:56:21 +0100

    debug 8 build
    Signed-off-by: openhabian <openhabian@openHABian>

--- build.bash
+++ build.bash
@@ -153,7 +153,8 @@ offline_install_modifications() {
     mount -o rw -t vfat "/dev/mapper/${loopPrefix}p1" "${mountFolder}/boot"
     set -x
     #systemd-nspawn --directory="$2" /opt/openhabian/build-image/offline-install-modifications.bash &> /dev/null
-    /opt/openhabian/build-image/offline-install-modifications.bash &> /dev/null
+    systemd-nspawn --directory="$2" /opt/openhabian/build-image/offline-install-modifications.bash 
+    #/opt/openhabian/build-image/offline-install-modifications.bash
     sync
     df -h "$mountFolder"
     df -h "${mountFolder}/boot"
