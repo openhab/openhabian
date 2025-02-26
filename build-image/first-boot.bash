8a576fcc3358e5b678178e380e5c141d6a22820e
Markus Storm <markus.storm@gmx.net>
2025-02-26 18:08:22 +0100

    -v Test -> -n
    Signed-off-by: Markus Storm <markus.storm@gmx.net>

--- build-image/first-boot.bash
+++ build-image/first-boot.bash
@@ -72,7 +72,7 @@ fi
 
 echo -n "$(timestamp) [openHABian] Changing default username ... "
 # shellcheck disable=SC2154
-if [[ -v ${userName} ]] || ! id "$defaultUserAndGroup" &> /dev/null || id "$userName" &> /dev/null; then
+if [[ -n ${userName} ]] || ! id "$defaultUserAndGroup" &> /dev/null || id "$userName" &> /dev/null; then
   echo "SKIPPED"
 else
   usermod -l "$userName" "$defaultUserAndGroup"
@@ -82,7 +82,7 @@ else
 fi
 echo -n "$(timestamp) [openHABian] Changing default password... "
 # shellcheck disable=SC2154
-if [[ -v ${userpw} ]]; then
+if [[ -n ${userpw} ]]; then
   echo "SKIPPED"
 else
   echo "${userName}:${userpw}" | chpasswd
