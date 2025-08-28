9111d11258594dd3e04d8e01dce79e9b75242b44
Patrick Götz <goetz.patrick@gmx.de>
2025-08-28 17:48:32 +0200

    another shellcheck comment on line 903

--- functions/packages.bash
+++ functions/packages.bash
@@ -900,7 +900,7 @@ setup_esphome_device_builder() {
       # Activate the virtual environment
       if [ $Setup_Mode = "installation" ] || [ $Setup_Mode = "update" ]; then
         echo "$(timestamp) [openHABian] Activating the virtual environment."
-        # shellcheck source=$ESPHOME_DIR/venv/bin/activate
+        # shellcheck source=/opt/esphome_device_builder/venv/bin/activate
         if ! source "$ESPHOME_DIR/venv/bin/activate"; then
           echo "$(timestamp) [openHABian] Error: Failed to activate a Python virtual environment."
             Setup_Mode="error"
