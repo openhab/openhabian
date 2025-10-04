87f4874aba1777367e10acf9278cee98a9d41399
Markus Storm <markus.storm@gmx.net>
2025-10-04 11:26:30 +0200

    sorted offline pkgs by name
    Signed-off-by: Markus Storm <markus.storm@gmx.net>

--- build-image/offline-install-modifications.bash
+++ build-image/offline-install-modifications.bash
@@ -33,22 +33,22 @@ adoptium_fetch_apt
 
 apt-get --quiet update
 apt-get --quiet upgrade --yes --force-confnew
-apt-get --quiet install --download-only --yes libc6 libstdc++6 zlib1g make \
+apt-get --quiet install --download-only --yes
   openhab openhab-addons \
   acl amanda-common amanda-server amanda-client apt-transport-https arping \
   avahi-daemon bash-completion bc bzip2 comitup coreutils curl \
   dnsutils dns-root-data dnsmasq-base dirmngr exim4 fontconfig gdisk git \
   htop iotop javascript-common jq \
-  libcairo2 libgudev-1.0-0 libjs-jquery libmbim-glib4 libmbim-proxy libmm-glib0 \
-  libndp0 libnm0 libpixman-1-0 libqmi-glib5 libqmi-proxy libteamdctl0 \
-  libxcb-render0 libxcb-shm0 libxrender1 \
-  mc mailutils modemmanager moreutils man-db multitail nano network-manager \
+  libc6 libcairo2 libgudev-1.0-0 libjs-jquery libmbim-glib4 libmbim-proxy \
+  libmm-glib0 libndp0 libnm0 libpixman-1-0 libqmi-glib5 libqmi-proxy libstdc++6 \
+  libteamdctl0 libxcb-render0 libxcb-shm0 libxrender1 \
+  make man-db mc mailutils modemmanager moreutils multitail nano network-manager \
   python3-blinker python3-cairo python3-click python3-colorama python3-flask \
   python3-itsdangerous python3-jinja2 python3-markupsafe \
   python3-networkmanager python3-pyinotify python3-simplejson python3-werkzeug \
   python3 python3-pip python3-wheel python3-setuptools \
   samba screen sysstat tailscale temurin-21-jre usbutils util-linux \
-  unzip vfu vim wget whiptail xz-utils zip
+  unzip vfu vim wget whiptail xz-utils zip zlib1g
 
 source /opt/openhabian/functions/nodejs-apps.bash
 nodejs_setup
