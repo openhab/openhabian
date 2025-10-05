2bebf9749ce03b351d15a087c8b8a78d1216b0e7
Markus Storm <markus.storm@gmx.net>
2025-10-05 07:15:24 +0200

    offline (pre-) install all basic packages (#2073)

--- functions/vpn.bash
+++ functions/vpn.bash
@@ -197,8 +197,8 @@ install_tailscale() {
   fi
 
   echo "$(timestamp) [openHABian] Installing tailscale VPN... "
-  curl -fsSL https://pkgs.tailscale.com/stable/debian/bookworm.noarmor.gpg | tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
-  curl -fsSL https://pkgs.tailscale.com/stable/debian/bookworm.tailscale-keyring.list | tee /etc/apt/sources.list.d/tailscale.list
+  curl -fsSL https://pkgs.tailscale.com/stable/debian/trixie.noarmor.gpg | tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
+  curl -fsSL https://pkgs.tailscale.com/stable/debian/trixie.tailscale-keyring.list | tee /etc/apt/sources.list.d/tailscale.list
   cond_redirect apt-get update -o DPkg::Lock::Timeout="$APTTIMEOUT"
 
   # Install tailscale
