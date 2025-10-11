73ea87cec3742572d8d2b893c2f5645b0029d05d
Patrick Götz <goetz.patrick@gmx.de>
2025-10-11 08:34:33 +0200

    fix ESPHome install script (#2059)
    * dynamic Config Path + Menü update
    
    Signed-off-by: Patrick Götz <goetz.patrick@gmx.de>

--- docs/openhabian.md
+++ docs/openhabian.md
@@ -254,6 +254,9 @@ Each of these are included as a part of `openhabian-config` menu option 20.
 -   [OWServer](https://owfs.org) - 1-Wire system of Dallas/Maxim
 -   [FIND](https://www.internalpositioning.com/) - Framework for Internal Navigation and Discovery
 -   Mi Flora MQTT daemon
+-   [ESPHome Device Builder](https://esphome.io/guides/getting_started_hassio.html#device-builder-interface) - Webinterface for easy configuration, installation and updating of ESPHome devices at [http://openhabian:6052](http://openhabian:6052) <br/>
+The ESPHome configuration files are secured by the openHABbian backup function. <br/>
+**Please note that automatic updates are not implemented in OpenHABian. <br/> &rarr; to do this, use the openhbaian-config update function in menu 20**
 -   Grott Proxy server for [Growatt binding](https://www.openhab.org/addons/bindings/growatt/#grott-application-installation-and-setup)
 
 ## First boot configuration
--- functions/menu.bash
+++ functions/menu.bash
@@ -131,8 +131,8 @@ show_main_menu() {
     "2D | Install EVCC"           "Deploy Electric Vehicle Charge Controller" \
     "   | Remove EVCC"            "Uninstall EVCC" \
     "   | Setup EVCC"             "Setup EVCC from command line (German only)" \
-    "2E | Install ESPHome dashboard"           "Deploy ESPHome dashboard" \
-    "   | Remove ESPHome dashboard"            "Uninstall ESPHome dashboard" \
+    "2E | Install ESPHome "       "Deploy / Update ESPHome Device Builder" \
+    "   | Remove ESPHome "        "Uninstall ESPHome Device Builder" \
     "2F | Install Grott"          "Install Grott Proxy server (for Growatt binding)" \
     "   | Remove Grott"           "Uninstall Grott Proxy server (for Growatt binding)" \
     3>&1 1>&2 2>&3)
@@ -160,8 +160,8 @@ show_main_menu() {
       2D\ *) install_evcc "install";;
       *Remove\ EVCC*) install_evcc "remove";;
       *Setup\ EVCC*) setup_evcc;;
-      2E\ *) install_esphomedashboard "install";;
-      *Remove\ ESPHome\ dashboard*) install_esphomedashboard "remove";;
+      2E\ *) setup_esphome_device_builder "install";;
+      *Remove\ ESPHome*) setup_esphome_device_builder "remove";;
       2F\ *) install_grott "install";;
       *Remove\ Grott*) install_grott "remove";;
       "") return 0 ;;
