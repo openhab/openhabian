8b484a8741e922e1228e0746987e2fa3d95dbe31
openhabian <openhabian@openHABian>
2025-10-13 16:35:42 +0200

    evcc 0.204.5 -> 0.206.1
    Signed-off-by: openhabian <openhabian@openHABian>

--- openhabian-setup.sh
+++ openhabian-setup.sh
@@ -14,8 +14,7 @@
 # überschreibt defaults in openhabian.conf
 
 openhabForcePkg=5.0.1-1		# war: 4.3.7-1
-#openhabForcePkg=4.3.7-1		# 4.3.7-1
-evccForcePkg=0.204.5		# war: 0.204.5
+evccForcePkg=0.206.1		# war: 0.204.5
 
 
 configFile="/etc/openhabian.conf"
