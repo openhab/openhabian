69710d002f52efcfd362073ebab6bb2a8c282b30
Markus Storm <markus.storm@gmx.net>
2025-10-16 16:25:59 +0200

    handle Wi-Fi in trixie like in bookworm (#2082)
    Signed-off-by: Markus Storm <markus.storm@gmx.net>

--- build-image/first-boot.bash
+++ build-image/first-boot.bash
@@ -41,8 +41,8 @@ if ! source "/opt/openhabian/functions/helpers.bash"; then echo "FAILED (source 
 if ! source "/opt/openhabian/functions/wifi.bash"; then echo "FAILED (source wifi)"; fail_inprogress; fi
 if source "/opt/openhabian/functions/openhabian.bash"; then echo "OK"; else echo "FAILED (source openhabian)"; fail_inprogress; fi
 
-if ! is_bookworm; then
-  rfkill unblock wifi   # Wi-Fi is blocked by Raspi OS default since bullseye(?)
+if ! is_trixie || ! is_bookworm; then
+  rfkill unblock wifi   # Wi-Fi is blocked by Raspi OS default since bullseye
 fi
 webserver=/boot/webserver.bash
 [[ -f /boot/firmware/webserver.bash ]] && ln -s /boot/firmware/webserver.bash "$webserver"
@@ -102,7 +102,7 @@ hotSpot=${hotspot:-enable}
 wifiSSID="$wifi_ssid"
 # shellcheck source=/etc/openhabian.conf disable=SC2154
 wifiPassword="$wifi_password"
-if is_pi && is_bookworm; then
+if is_trixie || is_bookworm && is_pi; then	# attention no brackets => left-associative ordering so put && last
   echo -n "$(timestamp) [openHABian] Setting up NetworkManager and Wi-Fi connection... "
   systemctl enable --now NetworkManager
   nmcli r wifi on
