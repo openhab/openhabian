6f91aab4662b4d642eb8764af16ff510d452aaa3
Markus Storm <markus.storm@gmx.net>
2025-10-17 08:30:08 +0200

    no -S on curl for tailscale
    Signed-off-by: Markus Storm <markus.storm@gmx.net>

--- build-image/offline-install-modifications.bash
+++ build-image/offline-install-modifications.bash
@@ -25,8 +25,10 @@ if [[ ! -f ${comituprepofile} ]]; then
 fi
 
 # tailscale VPN
-curl -fsSL https://pkgs.tailscale.com/stable/debian/trixie.noarmor.gpg | tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
-curl -fsSL https://pkgs.tailscale.com/stable/debian/trixie.tailscale-keyring.list | tee /etc/apt/sources.list.d/tailscale.list
+#curl -fsSL https://pkgs.tailscale.com/stable/debian/trixie.noarmor.gpg | tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
+#curl -fsSL https://pkgs.tailscale.com/stable/debian/trixie.tailscale-keyring.list | tee /etc/apt/sources.list.d/tailscale.list
+curl -fsL https://pkgs.tailscale.com/stable/debian/trixie.noarmor.gpg | tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
+curl -fsL https://pkgs.tailscale.com/stable/debian/trixie.tailscale-keyring.list | tee /etc/apt/sources.list.d/tailscale.list
 
 # OpenEMS JVM 21 32bit
 adoptium_fetch_apt 21
