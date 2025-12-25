4f96a52491517d892a13f9e1bfa4cb17c7d006ce
Markus Storm <markus.storm@gmx.net>
2025-12-25 15:51:22 +0100

    split homegear repo x86 vs RaspiOS 64bit
    Signed-off-by: Markus Storm <markus.storm@gmx.net>

--- functions/packages.bash
+++ functions/packages.bash
@@ -222,12 +222,17 @@ homegear_setup() {
 
 
   # Add Homegear's repository to APT - need to use testing repo
-  if ! is_pi || [[ "$(dpkg --print-architecture)" == 'arm64' ]]; then
-    # 64-bit Raspberry Pi OS:
-    echo 'deb [signed-by=/usr/share/keyrings/homegear-archive-keyring.gpg] https://apt.homegear.eu/debian/bookworm/homegear/testing/ bookworm main' > /etc/apt/sources.list.d/homegear.list
+  if ! is_pi; then
+    # x86:
+    echo 'deb [signed-by=/usr/share/keyrings/homegear-archive-keyring.gpg] https://apt.homegear.eu/debian/bookworm/homegear/stable/ bookworm main' > /etc/apt/sources.list.d/homegear.list
   else
-    # 32-bit Raspberry Pi OS
-    echo 'deb [signed-by=/usr/share/keyrings/homegear-archive-keyring.gpg] https://apt.homegear.eu/raspberry_pi_os/bookworm/homegear/testing/ bookworm main' > /etc/apt/sources.list.d/homegear.list
+    if [[ "$(dpkg --print-architecture)" == 'arm64' ]]; then
+      # 64-bit Raspberry Pi OS:
+      echo 'deb [signed-by=/usr/share/keyrings/homegear-archive-keyring.gpg] https://apt.homegear.eu/debian/bookworm/homegear/testing/ bookworm main' > /etc/apt/sources.list.d/homegear.list
+    else
+      # 32-bit Raspberry Pi OS
+      echo 'deb [signed-by=/usr/share/keyrings/homegear-archive-keyring.gpg] https://apt.homegear.eu/raspberry_pi_os/bookworm/homegear/testing/ bookworm main' > /etc/apt/sources.list.d/homegear.list
+    fi
   fi
   echo -n "$(timestamp) [openHABian] Installing Homegear... "
   if ! cond_redirect apt-get update; then echo "FAILED (update apt lists)"; return 1; fi
